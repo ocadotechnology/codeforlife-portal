9e88c420fee1409a6182d688aa40da0bff0b9c77
Robert Brignull <robert.brignull@ocado.com>
2014-07-24 14:45:13 +0100

    Added HTML for the rest of the forms and a rather ugly landing page to direct you to the right one.

--- portal/urls.py
+++ portal/urls.py
@@ -10,7 +10,9 @@ js_info_dict = {
 
 urlpatterns = patterns('',
     url(r'^$', 'portal.views.login'),
-    url(r'^login$', 'portal.views.login'),
+    url(r'^teacher/signup$', 'portal.views.teacher_signup'),
+    url(r'^teacher/login$', 'portal.views.teacher_login'),
+    url(r'^student/login$', 'portal.views.student_login'),
     url(r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
 )
--- portal/views.py
+++ portal/views.py
@@ -4,3 +4,12 @@ from django.core.mail import send_mail, BadHeaderError
 
 def login(request):
 	return render(request, 'portal/login.html', {})
+
+def teacher_signup(request):
+	return render(request, 'portal/teacher_signup.html', {})
+
+def teacher_login(request):
+	return render(request, 'portal/teacher_login.html', {})
+
+def student_login(request):
+	return render(request, 'portal/student_login.html', {})
