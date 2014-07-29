062f9ea2f032589a93bffeb7d9c2c97e0ec61355
Joseph Zammit <josephezammit@gmail.com>
2014-07-29 12:43:10 +0100

    Migration merge

---
+++ portal/migrations/0011_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portal', '0009_teacher_pending_join_request'),
+        ('portal', '0010_auto_20140729_1030'),
+    ]
+
+    operations = [
+    ]
