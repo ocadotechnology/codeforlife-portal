528c63e80a4bb645ac8dd4de09f14c031612958e
Joseph Zammit <josephezammit@gmail.com>
2014-08-05 17:30:13 +0100

    Added package data to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 setup(name='portal',
       version='1.0',
       packages=find_packages(),
+      include_package_data=True,
       install_requires = [
         'django-appconf==0.6',
         'django-casper==0.0.2',
