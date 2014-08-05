e564d6f589bb8212cb5669887f56b33650831ffd
Joseph Zammit <josephezammit@gmail.com>
2014-08-05 18:06:41 +0100

    First steps to having deployable cfl project - currently just with portal as installed app

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# To be used to docker deployment environment
+export DEPLOYMENT=1
+./manage.py collectstatic --noinput
+./manage.py migrate
+# flush memcache
+echo "from django.core.cache import cache; cache.clear()" | ./manage.py shell
+appcfg.py update --authenticate_service_account $DEPLOYMENT_CONFIG
---
+++ deploy/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+js_info_dict = {
+    'packages': ('conf.locale',),
+}
+
+urlpatterns = patterns('',
+    url(r'^/', include('portal.urls')),
+    url(r'^', include('portal.urls')),
+)
