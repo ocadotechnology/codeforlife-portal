63df0e15e7dfcf19692f4fad05dd1d52c48b238b
Joseph Zammit <josephezammit@gmail.com>
2014-08-06 11:29:01 +0100

    Added autoconfig

---
+++ portal/autoconfig.py
@@ -0,0 +1,17 @@
+'''Portal autoconfig'''
+
+SETTINGS = {
+    'INSTALLED_APPS': [
+        'portal',
+        'django.contrib.admin',
+        'django.contrib.admindocs',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'rest_framework',
+        'captcha',
+        'jquery',
+    ],
+}
