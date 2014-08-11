801b0f24adfc02e10afb7bf56408e7c07f71534c
Paul Heideman <paul.heideman@ocado.com>
2014-08-11 15:24:32 +0100

    Changed mimetype to content_type in basic auth middleware

--- deploy/middleware/basicauth.py
+++ deploy/middleware/basicauth.py
@@ -4,7 +4,7 @@ from django.conf import settings
 class BasicAuthMiddleware:
     def unauthed(self):
         response = HttpResponse("""<html><title>Auth required</title><body>
-                                <h1>Authorization Required</h1></body></html>""", mimetype="text/html")
+                                <h1>Authorization Required</h1></body></html>""", content_type="text/html")
         response['WWW-Authenticate'] = 'Basic realm="' + settings.BASE_URL + '"'
         response.status_code = 401
         return response
