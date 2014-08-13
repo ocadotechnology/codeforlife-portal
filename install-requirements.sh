eacb51f68527448b10ce9f5ba66b72f071b4003f
Robert Brignull <robert.brignull@ocado.com>
2014-08-13 11:52:35 +0100

    Improved local-setup, added install-requirements scripts.

---
+++ install-requirements.sh
@@ -0,0 +1,7 @@
+source VIRTUALENV/bin/activate
+
+pip install https://www.djangoproject.com/download/1.7c1/tarball/
+pip install https://bitbucket.org/rptlab/reportlab/get/tip.zip
+
+pip install -r ../ocargo/requirements.txt
+pip install -r ../portal/requirements.txt
--- local-setup.sh
+++ local-setup.sh
@@ -19,30 +19,30 @@ echo "Cloning other git repositories"
 cd ..
 PROJECT_PATH=`pwd`
 
-git clone https://github.com/ocadotechnology/ocargo ocargo
-git clone https://github.com/ocadotechnology/codeforlife-portal portal
+if [ ! -d ocargo ]; then
+    git clone https://github.com/ocadotechnology/ocargo ocargo
+fi
 
-# create local requirements
+if [ ! -d portal ]; then
+    git clone https://github.com/ocadotechnology/codeforlife-portal portal
+fi
+
+# install requirements
 cd ${DEPLOY_PATH}
-echo "Creating local-requirements.txt"
-echo "https://www.djangoproject.com/download/1.7c1/tarball/
-git+file://${PROJECT_PATH}/portal#egg=portal
-git+file://${PROJECT_PATH}/ocargo#egg=game" > local-requirements.txt
+./install-requirements.sh
 
-# install requirements normally to get all dependencies
-pip install -r local-requirements.txt
-
-# install sass, (to be able to switch to and from prod requirements.txt)
-sudo gem install sass --version "3.3.4"
-
-
-
-# now delete and change to symlinks,
+# now change to symlinks,
 # this means that local changes affect the running server immediately
 # without the need to install requirements again or restart the server
 rm -rf VIRTUALENV/lib/python2.7/site-packages/{game,portal}
 ln -sv ${PROJECT_PATH}/ocargo/game VIRTUALENV/lib/python2.7/site-packages/game
+ln -sv ${PROJECT_PATH}/ocargo/nuit VIRTUALENV/lib/python2.7/site-packages/nuit
+ln -sv ${PROJECT_PATH}/ocargo/reports VIRTUALENV/lib/python2.7/site-packages/reports
+ln -sv ${PROJECT_PATH}/ocargo/website VIRTUALENV/lib/python2.7/site-packages/website
 ln -sv ${PROJECT_PATH}/portal/portal VIRTUALENV/lib/python2.7/site-packages/portal
+
+# install sass, (to be able to switch to and from prod requirements.txt)
+sudo gem install sass --version "3.3.4"
 
 
 
@@ -52,10 +52,9 @@ touch ~/.bashrc
 echo "alias vac='source VIRTUALENV/bin/activate'" >> ~/.bashrc
 echo "alias rs='vac && VIRTUALENV/bin/python manage.py migrate && VIRTUALENV/bin/python manage.py collectstatic --noinput && VIRTUALENV/bin/python manage.py runserver'" >> ~/.bashrc
 
-source ~/.bashrc
 
 
-
+echo "run 'source ~/.bashrc' to pick the new aliases"
 echo "run 'vac' or 'source VIRTUALENV/bin/activate' to activate your virtualenv, and run 'deactivate' to deactivate it"
 echo "then run 'rs' or './manage.py runserver' to run a local runserver"
 echo "then you can navigate to http://localhost:8000"
