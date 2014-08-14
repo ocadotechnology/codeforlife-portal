a2ab7089b64fac5fef092f02fb6b624eb0b74219
Joseph Zammit <josephezammit@gmail.com>
2014-08-14 11:57:27 +0100

    Updated ContactForm on forms.py

--- portal/forms.py
+++ portal/forms.py
@@ -575,6 +575,7 @@ class StudentJoinOrganisationForm(forms.Form):
         return self.cleaned_data
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Name', widget=forms.TextInput(attrs={'placeholder': 'Name'}))
-    email = forms.EmailField(label='Email address', widget=forms.TextInput(attrs={'placeholder': 'Email address'}))
-    message = forms.CharField(label='Message', widget=forms.Textarea(attrs={'placeholder': 'Message'}))
+    name = forms.CharField(label='Name', max_length=100, widget=forms.TextInput(attrs={'class': 'contactField'}))
+    telephone = forms.CharField(label='Telephone', max_length=50, widget=forms.TextInput(attrs={'class': 'contactField'}))
+    email = forms.EmailField(label='Email address', widget=forms.TextInput(attrs={'class': 'contactField'}))
+    message = forms.CharField(label='Message', max_length=250, widget=forms.Textarea(attrs={'class': 'contactField'}))
