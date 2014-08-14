84ee4ca2b86720ffbd582c84ce8c52eb9a82a71b
Joseph Zammit <josephezammit@gmail.com>
2014-08-14 16:58:28 +0100

    A number of copy updates

--- portal/forms.py
+++ portal/forms.py
@@ -107,7 +107,7 @@ class TeacherSignupForm(forms.Form):
     title = forms.ChoiceField(label='Title', choices=choices, widget=forms.Select(attrs={'placeholder': 'Title', 'class': 'wide'}))
     first_name = forms.CharField(label='First name', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'First name'}))
     last_name = forms.CharField(label='Last name', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Last name'}))
-    email = forms.EmailField(label='Email address', widget=forms.TextInput(attrs={'placeholder': 'Email Address'}))
+    email = forms.EmailField(label='Email address', widget=forms.TextInput(attrs={'placeholder': 'Professional Email Address'}))
     password = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
     confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'placeholder': 'Confirm password'}))
 
