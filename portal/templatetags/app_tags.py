753244b4a06f4fbdde1354255a9868c2eed996b1
Joseph Zammit <josephezammit@gmail.com>
2014-08-14 08:59:03 +0100

    Added template tags for testing if logged in and for making user's name into appropriate string (depends on if teacher or student, and limits to 20 chars total before truncating with ...

--- portal/templatetags/app_tags.py
+++ portal/templatetags/app_tags.py
@@ -12,3 +12,28 @@ def emaildomain(email):
 @register.filter(name='has_2FA')
 def has_2FA(u):
     return default_device(u)
+
+@register.filter(name='is_logged_in')
+def is_logged_in(u):
+    return u.is_authenticated() and (not default_device(u) or u.is_verified())
+
+@register.filter(name='make_into_username')
+def make_into_username(u):
+    if hasattr(u, 'userprofile'):
+        username = ''
+        if hasattr(u.userprofile, 'teacher'):
+            username = u.userprofile.teacher.title + ' ' + u.first_name[:1] + ' ' + u.last_name
+        if hasattr(u.userprofile, 'student'):
+            username = u.first_name
+
+    if len(username) > 20:
+        username = username[:18] + '...'
+    return username
+
+@register.filter(name='is_logged_in_as_teacher')
+def is_logged_in_as_teacher(u):
+    return is_logged_in(u) and hasattr(u.userprofile, 'teacher')
+
+@register.filter(name='is_logged_in_as_student')
+def is_logged_in_as_student(u):
+    return is_logged_in(u) and hasattr(u.userprofile, 'student')
