b79b6eecec9e0044ab2b0721a375e3a2cb215d83
Robert Brignull <robert.brignull@ocado.com>
2014-08-14 17:25:02 +0100

    Modified local-setup install-requirements because of new ratelimit app. Run ./install-requirements to get everything working.

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,3 +1,12 @@
+# find out where we are
+DEPLOY_PATH=`pwd`
+cd ..
+PROJECT_PATH=`pwd`
+cd ${DEPLOY_PATH}
+
+# create virtual env
+rm -rf VIRTUALENV
+virtualenv VIRTUALENV
 source VIRTUALENV/bin/activate
 
 pip install https://www.djangoproject.com/download/1.7c1/tarball/
@@ -5,3 +14,14 @@ pip install https://bitbucket.org/rptlab/reportlab/get/tip.zip
 
 pip install -r ../ocargo/requirements.txt
 pip install -r ../portal/requirements.txt
+
+# now change to symlinks,
+# this means that local changes affect the running server immediately
+# without the need to install requirements again or restart the server
+rm -rf VIRTUALENV/lib/python2.7/site-packages/{game,portal}
+ln -sv ${PROJECT_PATH}/ocargo/game VIRTUALENV/lib/python2.7/site-packages/game
+ln -sv ${PROJECT_PATH}/ocargo/nuit VIRTUALENV/lib/python2.7/site-packages/nuit
+ln -sv ${PROJECT_PATH}/ocargo/reports VIRTUALENV/lib/python2.7/site-packages/reports
+ln -sv ${PROJECT_PATH}/ocargo/website VIRTUALENV/lib/python2.7/site-packages/website
+ln -sv ${PROJECT_PATH}/portal/portal VIRTUALENV/lib/python2.7/site-packages/portal
+ln -sv ${PROJECT_PATH}/portal/ratelimit VIRTUALENV/lib/python2.7/site-packages/ratelimit
--- local-setup.sh
+++ local-setup.sh
@@ -9,15 +9,9 @@ DEPLOY_PATH=`pwd`
 # install virtualenv
 sudo pip install virtualenv
 
-# create virtual env
-rm -rf VIRTUALENV
-virtualenv VIRTUALENV
-source VIRTUALENV/bin/activate
-
 # clone the other repositories
 echo "Cloning other git repositories"
 cd ..
-PROJECT_PATH=`pwd`
 
 if [ ! -d ocargo ]; then
     git clone https://github.com/ocadotechnology/ocargo ocargo
@@ -27,19 +21,9 @@ if [ ! -d portal ]; then
     git clone https://github.com/ocadotechnology/codeforlife-portal portal
 fi
 
-# install requirements
+# create virtualenv and install requirements
 cd ${DEPLOY_PATH}
 ./install-requirements.sh
-
-# now change to symlinks,
-# this means that local changes affect the running server immediately
-# without the need to install requirements again or restart the server
-rm -rf VIRTUALENV/lib/python2.7/site-packages/{game,portal}
-ln -sv ${PROJECT_PATH}/ocargo/game VIRTUALENV/lib/python2.7/site-packages/game
-ln -sv ${PROJECT_PATH}/ocargo/nuit VIRTUALENV/lib/python2.7/site-packages/nuit
-ln -sv ${PROJECT_PATH}/ocargo/reports VIRTUALENV/lib/python2.7/site-packages/reports
-ln -sv ${PROJECT_PATH}/ocargo/website VIRTUALENV/lib/python2.7/site-packages/website
-ln -sv ${PROJECT_PATH}/portal/portal VIRTUALENV/lib/python2.7/site-packages/portal
 
 # install sass, (to be able to switch to and from prod requirements.txt)
 sudo gem install sass --version "3.3.4"
