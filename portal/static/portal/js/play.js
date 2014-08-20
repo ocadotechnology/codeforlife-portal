6b924d2cabe5f21197d1c3f9f366e49f248de448
Joseph Zammit <josephezammit@gmail.com>
2014-08-20 09:47:36 +0100

    Minor amends

--- portal/static/portal/js/play.js
+++ portal/static/portal/js/play.js
@@ -13,6 +13,23 @@ function init() {
     $('#signup-warning').hide()
     $('#signup-form').show()
   });
+
+  $('#register-link').click(function() {
+    // TODO test if logged in and show popup box saying logged in, please log out first, etc.
+    $('#signup-warning').hide()
+    $('#signup-form').show()
+  });
+
+  $('#solo-login-link').click(function() {
+    // TODO test if logged in and show popup box saying logged in, please log out first, etc.
+    $('#switchToSolo').click()
+  });
+
+  $('#school-login-link').click(function() {
+    // TODO test if logged in and show popup box saying logged in, please log out first, etc.
+    $('#switchToSchool').click()
+  });
+
 };
 
 $( document ).ready(function() {
