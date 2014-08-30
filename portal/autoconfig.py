c47b5683774aec20734dd0c0ebf7c410aacca25d
Paul Heideman <paul.heideman@ocado.com>
2014-08-30 09:51:36 +0100

    Made recaptcha keys environment variables

--- portal/autoconfig.py
+++ portal/autoconfig.py
@@ -1,4 +1,5 @@
 '''Portal autoconfig'''
+import os
 
 SETTINGS = {
     'INSTALLED_APPS': [
@@ -31,9 +32,9 @@ SETTINGS = {
 
     'CLOUD_STORAGE_PREFIX': 'http://storage.googleapis.com/codeforlife-assets/',
 
-    'RECAPTCHA_PUBLIC_KEY': '6LcGkPgSAAAAAMXqgeeiDsp2XJ0EBlTOHMZavp8t',
-    'RECAPTCHA_PRIVATE_KEY': '6LcGkPgSAAAAACEZHXbIjB6IqEtXy4ljDptHnQej',
-    
+    'RECAPTCHA_PUBLIC_KEY': os.getenv('RECAPTCHA_PUBLIC_KEY', None),
+    'RECAPTCHA_PRIVATE_KEY': os.getenv('RECAPTCHA_PRIVATE_KEY', None),
+
     'LOGGING': {
         'version': 1,
         'disable_existing_loggers': False,
