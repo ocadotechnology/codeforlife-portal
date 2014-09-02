b6b970454d0aebbded092da0607c013790d7ee05
Joseph Zammit <josephezammit@gmail.com>
2014-09-02 11:47:25 +0100

    Added what hopefully will give us support for images in emails...

--- portal/email_sender.py
+++ portal/email_sender.py
@@ -1,17 +1,36 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template import Context, loader
+from email.MIMEImage import MIMEImage
 
 
 def send_email(sender, recipients, subject, text_content, html_content=None, plaintext_template='email.txt', html_template='email.html'):
-	plaintext = loader.get_template(plaintext_template)
-	html = loader.get_template(html_template)
-	plaintext_email_context = Context({ 'content' : text_content })
-	html_email_context = Context({ 'content' : text_content, 'website_url' : settings.CODEFORLIFE_WEBSITE })
-	if html_content:
-		html_email_context = Context({ 'content' : html_content, 'website_url' : settings.CODEFORLIFE_WEBSITE })
-	plaintext_body = plaintext.render(plaintext_email_context)
-	html_body = html.render(html_email_context)
-	message = EmailMultiAlternatives(subject, plaintext_body, sender, recipients)
-	message.attach_alternative(html_body, "text/html")
-	message.send()
+    # setup template images library
+    images=[['cfl_logo_blue_rounded.png','cfl-logo']]
+
+    # setup templates
+    plaintext = loader.get_template(plaintext_template)
+    html = loader.get_template(html_template)
+    plaintext_email_context = Context({ 'content' : text_content })
+    html_email_context = Context({ 'content' : text_content })
+    if html_content:
+        html_email_context = Context({ 'content' : html_content })
+
+    # render templates
+    plaintext_body = plaintext.render(plaintext_email_context)
+    html_body = html.render(html_email_context)
+
+    # make message using templates
+    message = EmailMultiAlternatives(subject, plaintext_body, sender, recipients)
+    message.attach_alternative(html_body, "text/html")
+
+    # add in template for templates to message
+    for img in images:
+      fp = open(settings.MEDIA_ROOT+img[0], 'rb')
+      msgImage = MIMEImage(fp.read())
+      fp.close()
+      msgImage.add_header('Content-ID', '<'+img[1]+'>')
+      message.attach(msgImage)
+
+    # send!
+    message.send()
