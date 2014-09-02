c69dd8933b4aa47fffe607b3ae533ccb616a2062
Joseph Zammit <josephezammit@gmail.com>
2014-09-02 15:59:03 +0100

    This probably won't work, but let's test it anyway

--- portal/email_sender.py
+++ portal/email_sender.py
@@ -6,14 +6,14 @@ from email.MIMEImage import MIMEImage
 
 def send_email(sender, recipients, subject, text_content, html_content=None, plaintext_template='email.txt', html_template='email.html'):
     # setup template images library, make into attachments
-    images=[['cfllogo.png','cfllogo.png']]
+    images=[['cfllogo.png','cfllogo']]
     attachments = []
     # add in template for templates to message
     for img in images:
         fp = open(settings.MEDIA_ROOT+img[0], 'rb')
         msgImage = MIMEImage(fp.read())
         fp.close()
-        msgImage.add_header('Content-ID', '<'+img[0]+'>')
+        msgImage.add_header('Content-ID', '<'+img[1]+'>')
         msgImage.add_header('Content-Disposition', 'inline', filename=img[0])
         attachments.append(msgImage)
 
