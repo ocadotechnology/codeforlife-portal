df2e93da7f34f570cf2adea21b137f311b5a88b3
Joseph Zammit <josephezammit@gmail.com>
2014-09-04 17:28:11 +0100

    Added migration to reset password using env variable

---
+++ portal/migrations/0034_data_viewing_user_fix.py
@@ -0,0 +1,20 @@
+from django.db import models, migrations
+from django.contrib.auth.hashers import make_password
+import os
+
+from django.contrib.auth.models import User
+
+def fix_users(apps, schema_editor):
+    data_user = User.objects.get(username='DATA_AGGREGATE')
+    data_user.set_password(os.getenv('DATA_AGGREGATE_PASSWORD', 'Password1'))
+    data_user.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+            ('portal', '0033_data_viewing_user'),
+    ]
+
+    operations = [
+            migrations.RunPython(fix_users),
+    ]
