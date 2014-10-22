d8ccad1a0fd3c6b90d194724c11a6f924a58c5d8
ruth.wall <ruth.wall@outlook.com>
2014-10-22 20:04:06 +0100

    #449 Ditching placeholders in favour of labels

--- portal/forms/teach.py
+++ portal/forms/teach.py
@@ -14,22 +14,17 @@ class TeacherSignupForm(forms.Form):
 
     title = forms.ChoiceField(
         label='Title', choices=choices,
-        widget=forms.Select(attrs={'placeholder': "Title", 'class': 'wide'}))
+        widget=forms.Select(attrs={'class': 'wide'}))
     first_name = forms.CharField(
-        label='First name', max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': "First name"}))
+        label='First name', max_length=100)
     last_name = forms.CharField(
-        label='Last name', max_length=100,
-        widget=forms.TextInput(attrs={'placeholder': "Last name"}))
+        label='Last name', max_length=100)
     email = forms.EmailField(
-        label='Email address',
-        widget=forms.TextInput(attrs={'placeholder': "Professional Email Address"}))
+        label='Email address', max_length=100)
     password = forms.CharField(
-        label='Password',
-        widget=forms.PasswordInput(attrs={'placeholder': "Password"}))
+        label='Password')
     confirm_password = forms.CharField(
-        label='Confirm Password',
-        widget=forms.PasswordInput(attrs={'placeholder': "Confirm password"}))
+        label='Confirm Password')
 
     def clean_email(self):
         email = self.cleaned_data.get('email', None)
