8592f0342fb6d4c19686a5cae91cde091fbce45e
ruth.wall <ruth.wall@outlook.com>
2014-11-05 18:53:34 +0000

    adding line breaks

--- portal/forms/teach.py
+++ portal/forms/teach.py
@@ -15,23 +15,36 @@ choices = [('Miss', 'Miss'), ('Mrs', 'Mrs'), ('Ms', 'Ms'), ('Mr', 'Mr'),
 class TeacherSignupForm(forms.Form):
 
     title = forms.ChoiceField(
-        label='Title', choices=choices,
-        widget=forms.Select(attrs={'class': 'wide'}))
+        label='Title',
+        choices=choices,
+        widget=forms.Select(
+            attrs={
+                'class': 'wide'
+            }
+        )
+    )
     first_name = forms.CharField(
-        label='First name', max_length=100,
-        widget=forms.TextInput())
+        label='First name',
+        max_length=100,
+        widget=forms.TextInput()
+    )
     last_name = forms.CharField(
-        label='Last name', max_length=100,
-        widget=forms.TextInput())
+        label='Last name',
+        max_length=100,
+        widget=forms.TextInput()
+    )
     email = forms.EmailField(
         label='Email address',
-        widget=forms.EmailInput())
+        widget=forms.EmailInput()
+    )
     password = forms.CharField(
         label='Password',
-        widget=forms.PasswordInput())
+        widget=forms.PasswordInput()
+    )
     confirm_password = forms.CharField(
         label='Confirm Password',
-        widget=forms.PasswordInput())
+        widget=forms.PasswordInput()
+    )
 
     def clean_email(self):
         email = self.cleaned_data.get('email', None)
