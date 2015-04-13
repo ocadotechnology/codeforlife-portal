fe39bd1816a588ed3c26cd9645c89fe84cdfe36d
Cherie Pun <cherie.cy.pun@gmail.com>
2015-04-13 16:57:17 +0100

    #508 - Added CharField country to School in portal/models.py

---
+++ portal/migrations/0042_school_country.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('portal', '0041_new_news'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='school',
+            name='country',
+            field=models.CharField(max_length=200, null=True, blank=True),
+            preserve_default=True,
+        ),
+    ]
