d2a6cd29f0bf03865227614426d523dd880f764d
Cherie Pun <cherie.cy.pun@gmail.com>
2015-05-01 16:27:35 +0100

    #508 - Added testcase to test the migration for creating country and populating it - base_test_migration sets up the testing db - test_migration_country creates data, perform migration and check results - import email is commented out on teacher.py as it is giving error that stops the test from running - generate_details() in utils/teacher.py and utils/organisation.py are changed to take in **kwargs for customised test objects

--- portal/migrations/0044_auto_20150430_0959.py
+++ portal/migrations/0044_auto_20150430_0959.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from portal.helpers import location
+from time import sleep
 
 class Migration(migrations.Migration):
 
+    # It will guess the country of the school using postcode, UK will be set if no result is returned by the API
     def populate_country(apps, schema_editor):
 
         School = apps.get_model("portal", "School")
