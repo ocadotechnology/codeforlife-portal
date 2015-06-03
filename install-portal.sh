2082631728c66a9a63857e5e86c5c349745cd353
Paul Heideman <paul.heideman@ocado.com>
2015-06-03 14:49:11 +0100

    Fixed missing version change

--- install-portal.sh
+++ install-portal.sh
@@ -4,7 +4,7 @@ ON_GOOGLE=$?
 if (( ! $ON_GOOGLE )); then
   cd /tmp
   curl -s https://www.googleapis.com/storage/v1/b/decent-digit-629.appspot.com/o/django-pandasso-1.7.1.tar.gz?alt=media \
-    -H "Authorization":"Bearer $(curl -s "http://metadata/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google" | jq -r .access_token)" > django-pandasso-1.5.1.tar.gz
+    -H "Authorization":"Bearer $(curl -s "http://metadata/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google" | jq -r .access_token)" > django-pandasso-1.7.1.tar.gz
   pip install django-pandasso-1.7.1.tar.gz
   cd -
 fi
