c467d22e00d3aead5ffac02010bfa086462f3720
Paul Heideman <paul.heideman@ocado.com>
2015-06-11 10:10:06 +0100

    Attempt at using lib folder and submodules folder instead of copying

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-pip install -r requirements.txt
-ssh-agent ./install-portal.sh
-ssh-agent ./install-rapid-router.sh
+pip install -U -t lib -r requirements.txt
+./install-portal.sh
+./install-rapid-router.sh
 echo "Following packages present:"
 pip freeze
 echo "--------------------------------------------------------------------------------"
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd /opt/codeforlife-deploy/
 ./build.sh
-ls -d /usr/local/lib/python2.7/dist-packages/* | grep -v info | grep -v PIL | xargs -i cp -R {} /opt/codeforlife-deploy/
 # To be used to docker deployment environment
 export DEPLOYMENT=1
 ./manage.py collectstatic --noinput
