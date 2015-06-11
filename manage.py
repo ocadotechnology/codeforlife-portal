8ba3c4ecf240d84289a5167ad9a0cd0b7f51afdb
Paul Heideman <paul.heideman@ocado.com>
2015-06-11 10:04:49 +0100

    Added site packages trick to main.py for app engine

--- manage.py
+++ manage.py
@@ -10,19 +10,20 @@ def add_other_packages(other_folder):
     import site
     import os.path
     import sys
- 
+
     sys.path, remainder = sys.path[:1], sys.path[1:]
     site.addsitedir(os.path.join(os.path.dirname(__file__), other_folder))
- 
+
     # Finally, we'll add the paths we removed back.
     sys.path.extend(remainder)
- 
 
-
-if __name__ == "__main__":
+def do_site_packages():
     add_other_packages('lib')
     add_other_packages('submodules/codeforlife-portal')
     add_other_packages('submodules/ocargo')
+
+if __name__ == "__main__":
+    do_site_packages()
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "deploy.settings")
 
     from django.core.management import execute_from_command_line
