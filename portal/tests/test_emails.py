ee4e91e049feb65df0ff532ddef312535bff5b4d
Celine Boudier <boudier.celine@gmail.com>
2016-03-21 17:25:42 +0000

    adding new users notification

--- portal/tests/test_emails.py
+++ portal/tests/test_emails.py
@@ -34,54 +34,16 @@
 # copyright notice and these terms. You must not misrepresent the origins of this
 # program; modified versions of the program must be marked as such and not
 # identified as the original program.
-from datetime import timedelta
-
-from django.utils import timezone
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
-from django.core.urlresolvers import reverse_lazy
-from django.contrib import messages as messages
-
-from portal.models import EmailVerification, UserProfile
-from portal.helpers.email import send_email, NOTIFICATION_EMAIL
-from portal.app_settings import CONTACT_FORM_EMAILS
+from django.test import Client
+from unittest import TestCase
+from django.core.urlresolvers import reverse
+from django.core import mail
 
 
-def verify_email(request, token):
-    verifications = EmailVerification.objects.filter(token=token)
-
-    if len(verifications) != 1:
-        return render(request, 'portal/email_verification_failed.html')
-
-    verification = verifications[0]
-
-    if verification.used or (verification.expiry - timezone.now()) < timedelta():
-        return render(request, 'portal/email_verification_failed.html')
-
-    verification.used = True
-    verification.save()
-
-    user = verification.user
-    user.awaiting_email_verification = False
-    user.save()
-
-    if verification.email:
-        user.user.email = verification.email
-        user.user.save()
-
-    messages.success(request, 'Your email address was successfully verified, please log in.')
-
-    if hasattr(user, 'student'):
-        return HttpResponseRedirect(reverse_lazy('play'))
-    elif hasattr(user, 'teacher'):
-        return HttpResponseRedirect(reverse_lazy('teach'))
-
-    # default to homepage if something goes wrong
-    return HttpResponseRedirect(reverse_lazy('home'))
-
-
-def send_new_users_report(request):
-    new_profiles_count = UserProfile.objects.filter(user__date_joined__gte=timezone.now() - timedelta(days=7)).count()
-    send_email(NOTIFICATION_EMAIL, CONTACT_FORM_EMAILS, "new users",
-               "There are %d new users this week!" % new_profiles_count)
-    return HttpResponse('success')
+class EmailTest(TestCase):
+    def test_send_new_users_numbers_email(self):
+        client = Client()
+        response = client.get(reverse('send_new_users_report'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(mail.outbox), 1)
+        mail.outbox = []
