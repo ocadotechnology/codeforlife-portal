3b5349f1aa00b04d854956d9c8f7c1dfd93a44c5
v.kalidasan <v.kalidasan@ocado.com>
2016-08-16 12:02:57 +0100

    codeclimate changes

--- portal/static/portal/js/solutionSelection.js
+++ portal/static/portal/js/solutionSelection.js
@@ -29,17 +29,17 @@
  */
 $(function() {
     $('#episodes').accordion({ collapsible: true, heightStyle: "content", active: false });
-    normalEpisodes = $('#episodes').children('h3').not('.customLevelsEpisode');
-    minOpacity = 0.1;
-    maxOpacity = 0.7;
-    numEpisodes = normalEpisodes.length;
-    background = $('body').css('background-color');
-    bg = {'r':236, 'g':234, 'b':238};
-    baseColor = {'r': 70, 'g':44, 'b': 213};
+    var normalEpisodes = $('#episodes').children('h3').not('.customLevelsEpisode');
+    var minOpacity = 0.1;
+    var maxOpacity = 0.7;
+    var numEpisodes = normalEpisodes.length;
+    var background = $('body').css('background-color');
+    var bg = {'r':236, 'g':234, 'b':238};
+    var baseColor = {'r': 70, 'g':44, 'b': 213};
     for (var i=0; i < numEpisodes; i++) {
-        opacity = minOpacity + i*(maxOpacity - minOpacity)/numEpisodes;
+        var opacity = minOpacity + i*(maxOpacity - minOpacity)/numEpisodes;
         var color = {'r':baseColor.r, 'g':baseColor.g, 'b':baseColor.b, 'a':opacity};
-        combinedColor = combineColors(bg, color);
+        var combinedColor = combineColors(bg, color);
         var newRGB = 'rgb('+Math.floor(combinedColor.r).toString()+', '+Math.floor(combinedColor.g).toString()+', '+Math.floor(combinedColor.b).toString()+')';
         normalEpisodes[i].style.background = newRGB;
     }
