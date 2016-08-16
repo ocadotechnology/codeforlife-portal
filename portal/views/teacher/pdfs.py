e07faaab305580588cd14975ba45dd24c9b019c6
Felix <f.stephenson@Ocado.com>
2016-08-16 14:39:45 +0100

    Teaching materials browser home page start and viewer page

---
+++ portal/views/teacher/pdfs.py
@@ -0,0 +1,64 @@
+PDF_DATA = {
+    "introduction_to_coding": {
+        "title": "Introduction to Coding",
+        "description": "This document shows how the progrssion through rapid router levels cover each stage of the computing curriculum for key stages one and two.",
+        "links": None,
+        "url": "general_resources/introduction_to_coding.pdf"
+    },
+
+    "levels_guide_1-50": {
+        "title": "Levels Guide 1 - 50",
+        "description": "Road maps for levels 1 to 50 of Rapid Router. You can find the maps for levels 51 - 109 below.",
+        "links": ["levels_guide_51-109"],
+        "url": "general_resources/levels_guide.pdf"
+    },
+
+    "glossary": {
+        "title": "Glossary",
+        "description": "A glossary of coding terminology, for all the key stages of the curriculum.",
+        "links": None,
+        "url": "general_resources/glossary.pdf"
+    },
+
+    "introduction_to_python": {
+        "title": "Introduction to Python in Rapid Router",
+        "description": "A summary of the python commands specific to Rapid Router as well as general python statements children will need to use.",
+        "links": None,
+        "url": "general_resources/UKS2-Intro_to_Python.pdf"
+    },
+
+    "levels_guide_51-109": {
+        "title": "Levels Guide 50 - 109",
+        "description": "Road maps for levels 51 to 109 of Rapid Router.",
+        "links": ["levels_guide_1-50"],
+        "url": "general_resources/UKS2-levels-guide.pdf"
+    },
+
+    "blockly_python_phrasebook": {
+        "title": "Blockly - Python Phrasebook",
+        "description": "A table showing the conversion from Blockly to Python.",
+        "links": None,
+        "url": "general_resources/UKS2-Python_Phrasebook.pdf"
+    },
+
+    "program_solutions_1-28": {
+        "title": "Program Solutions 1 - 28",
+        "description": "A table showing the best solutions to levels 1 to 28 of Rapid Router.",
+        "links": ["program_solutions_29-50", "program_solutions_51-109"],
+        "url": "KS1/program_solutions/program_solutions_table.pdf"
+    },
+
+    "program_solutions_29-50": {
+        "title": "Program Solutions 29 - 50",
+        "description": "A table showing the best solutions to levels 29 to 50 of Rapid Router.",
+        "links": ["program_solutions_1-28", "program_solutions_51-109"],
+        "url": "KS2/program_solutions/program_solutions_table.pdf"
+    },
+
+    "program_solutions_51-109": {
+        "title": "Program Solutions 51 - 109",
+        "description": "A table showing the best solutions to levels 51 to 109 of Rapid Router.",
+        "links": ["program_solutions_1-28", "program_solutions_29-50"],
+        "url": "python/program_solutions/UKS2-PST-Program-Solutions-Table.pdf"
+    }
+}
