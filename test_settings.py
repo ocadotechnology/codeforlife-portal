ec8e2fc3cb414e896ea4a6e8153448749953bad8
Joshua Blake <joshua.blake@ocado.com>
2016-08-24 09:01:53 +0100

    Fix travis build.

--- test_settings.py
+++ test_settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = ['portal']
 PIPELINE_ENABLED = False
 ROOT_URLCONF = 'django_autoconfig.autourlconf'
 STATIC_ROOT = '.tests_static/'
+SECRET_KEY = 'bad_test_secret'
 
 from django_autoconfig.autoconfig import configure_settings
 configure_settings(globals())
