03a146d4dc4962f707229c8cf19ba364a2a757a8
Piotr Repetowski <piotr.repetowski@gmail.com>
2017-05-07 23:03:08 +0200

    Allow changing the class_name in tests when creating a class

--- portal/tests/utils/classes.py
+++ portal/tests/utils/classes.py
@@ -55,8 +55,11 @@ def generate_email(name):
     return name.replace(' ', '_') + '@codeforlife.com'
 
 
-def create_class_directly(teacher_email):
+def create_class_directly(teacher_email, class_name=None):
     name, accesss_code = generate_details()
+
+    if class_name is not None:
+        name = class_name
 
     teacher = Teacher.objects.get(new_user__email=teacher_email)
 
