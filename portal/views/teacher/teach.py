0e66b2d4cdd517599d75bacb435981d4286de789
Florian <florian.aucomte@kcl.ac.uk>
2017-08-23 11:17:02 +0100

    Removed teacher solutions from project.

--- portal/views/teacher/teach.py
+++ portal/views/teacher/teach.py
@@ -117,21 +117,6 @@ def materials_viewer(request, pdf_name):
 
 @login_required(login_url=reverse_lazy('login_view'))
 @user_passes_test(logged_in_as_teacher, login_url=reverse_lazy('login_view'))
-def default_solution(request, levelName):
-    if 80 <= int(levelName) <= 91:
-        return render(request, 'portal/teach/teacher_solutionPY.html', {'levelName': levelName})
-    else:
-        return render(request, 'portal/teach/teacher_solution.html', {'levelName': levelName})
-
-
-@login_required(login_url=reverse_lazy('login_view'))
-@user_passes_test(logged_in_as_teacher, login_url=reverse_lazy('login_view'))
-def teacher_level_solutions(request):
-    return render(request, 'portal/teach/teacher_level_solutions.html')
-
-
-@login_required(login_url=reverse_lazy('login_view'))
-@user_passes_test(logged_in_as_teacher, login_url=reverse_lazy('login_view'))
 def teacher_classes(request):
     teacher = request.user.new_teacher
     requests = Student.objects.filter(pending_class_request__teacher=teacher)
