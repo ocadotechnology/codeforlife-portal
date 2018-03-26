e7ad1e0e907d0073c2e2d9e34ea7e1787182a109
Celine Boudier <boudier.celine@gmail.com>
2018-03-26 16:13:12 +0100

    Got rid of django-cms - fixes #678 (#686)
    * Got rid of django-cms - fixes #678
    
    * added block head back

--- setup.py
+++ setup.py
@@ -29,28 +29,10 @@ setup(name='codeforlife-portal',
         'urllib3==1.10.4',
         'requests==2.7.0',
 
-        'django-cms==3.1.2',
-
         'django-classy-tags==0.6.1',
         'django-treebeard==3.0',
         'django-sekizai==0.8.2',
-        'djangocms-admin-style==0.2.8',
 
-        'djangocms-text-ckeditor==2.6.0',
-        'djangocms-link==1.6.2',
-        'djangocms-snippet==1.5',
-        'djangocms-style==1.5',
-        'djangocms-column==1.5',
-        'djangocms-grid==1.2',
-        'djangocms-oembed==0.5',
-        'djangocms-table==1.2',
-        'djangocms-file==0.1',
-        'djangocms_flash==0.2.0',
-        'djangocms_googlemap==0.3',
-        'djangocms_inherit==0.1',
-        'djangocms_picture==0.1',
-        'djangocms_teaser==0.1',
-        'djangocms_video==0.1',
         'django-online-status==0.1.0',
 
 
