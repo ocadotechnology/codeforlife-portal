0e5a30402a4402155ae740f66122533be1132157
Ria Jha <ria.jha@ocado.com>
2018-04-16 16:06:37 +0100

    Added more print statements for debugging

--- portal/tests/test_newsletter_footer.py
+++ portal/tests/test_newsletter_footer.py
@@ -7,9 +7,10 @@ class NewsletterFooter(BaseTest):
     def test_footer_signup_successful(self):
         # Test signup with valid email address
         page = self.go_to_homepage()
-        print page.browser.page_source.encode("utf-8")
         valid_email = "test@example.com"
+        print "About to send successful form"
         page.newsletter_singup(valid_email)
+        print "Sent valid post req"
         print page.browser.page_source.encode("utf-8")
         assert is_newsletter_signup_successful_message_showing(selenium)
 
@@ -17,9 +18,10 @@ class NewsletterFooter(BaseTest):
     def test_footer_signup_fail(self):
         # Test signup with invalid email address
         page = self.go_to_homepage()
-        print page.browser.page_source.encode("utf-8")
         invalid_email = "invalid_email"
+        print "About to send invalid email"
         page.newsletter_singup(invalid_email)
+        print "Sent invalid email"
         print page.browser.page_source.encode("utf-8")
         assert is_newsletter_signup_fail_message_showing(selenium)
 
