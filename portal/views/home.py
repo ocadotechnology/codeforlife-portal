9fc76082d268d965f199a334f7cbbc59af5a1f66
Unknown <ria.jha@ocado.com>
2018-04-19 15:35:40 +0100

    More changes for testing

--- portal/views/home.py
+++ portal/views/home.py
@@ -403,10 +403,10 @@ def process_newsletter_form(request):
         if newsletter_form.is_valid():
             user_email = newsletter_form.cleaned_data['email']
             add_to_salesforce("", "", user_email)
-            messages.success(request, 'Thank you for signing up!')
-            return render(request, 'portal/confirm_news_signup.html')
-        messages.error(request, 'Invalid email address. Please try again.', extra_tags='sub-nav--warning')
-        return render(request, 'portal/confirm_news_signup.html')
+            #messages.success(request, 'Thank you for signing up!')
+            return render(request, 'portal/confirm_news_signup.html', {'confirmation_message': 'Success!'})
+        #messages.error(request, 'Invalid email address. Please try again.', extra_tags='sub-nav--warning')
+        return render(request, 'portal/confirm_news_signup.html', {'confirmation_message': 'Failure'})
 
 
 def home(request):
