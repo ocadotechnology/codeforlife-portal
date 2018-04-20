90c12eb16f6d9b762c4f21de7f5faa9eefbe3820
riaJha97 <37871616+riaJha97@users.noreply.github.com>
2018-04-20 15:57:32 +0100

    Email opt in using render (#714)
    * Began writing code for the newsletter sign up button
    
    * Attempt at newsletter signup in footer
    
    * Finished working on the front end of footer sign up
    
    * Removed add_email_to_salesforce function. Need to change the signaturte of the original function
    
    * Changed signature of add_to_salesforce function
    
    * Reformatted code
    
    * Finished writing tests
    
    * Reformatted code
    
    * Changed newsletter_signup function
    
    * Added reference to context processors in test_settings.py
    
    * Added a click to submit newsletter form
    
    * Added print statements for debugging
    
    * Added more print statements for debugging
    
    * Deleted test_newsletter_footer
    
    * Switched to unit tests for the newsletter footer
    
    * Reformatted code
    
    * Reformatted code
    
    * Changed search for 'warning' to search for 'error' in base.html
    
    * Changed variable name to response
    
    * Movewd newsletter_form import up with other portal imports in home.py
    
    * Merged rules for newsletter footer css
    
    * Changed is_logged_in for when users don't exist
    
    * Added ref to context_processor in autoconfig
    
    * Switched to javascript for page redirection
    
    * removed incorrect getElementbyId statement
    
    * Added a case for get req. just in case
    
    * Tried to get the relative path for redirection
    
    * Changed url pattern of news_signup
    
    * Removed redirection for testing
    
    * More changes for testing
    
    * Using render instead of redirection after signup
    
    * Changed test cases so they match the new version of process_newsletter_form
    
    * Fixed merge conflict
    
    * Fixed syntax error
    
    * Reformatted code to follow pep8

--- portal/views/home.py
+++ portal/views/home.py
@@ -403,13 +403,9 @@ def process_newsletter_form(request):
         if newsletter_form.is_valid():
             user_email = newsletter_form.cleaned_data['email']
             add_to_salesforce("", "", user_email)
-            messages.success(request, 'Thank you for signing up!')
-            next = request.POST.get('URL')
-            return HttpResponseRedirect(next)
+            return render(request, 'portal/confirm_news_signup.html')
 
-        messages.error(request, 'Invalid email address. Please try again.', extra_tags='sub-nav--warning')
-        next = request.POST.get('URL')
-        return HttpResponseRedirect(next)
+        return render(request, 'portal/news_signup_fail.html')
 
 
 def home(request):
