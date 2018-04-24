637a73674e368e3038a7a7ce3c776952e6e45f21
Celine Boudier <boudier.celine@gmail.com>
2018-04-24 12:11:50 +0100

    Revert "Email opt in using render (#714)"
    This reverts commit 90c12eb16f6d9b762c4f21de7f5faa9eefbe3820.

--- portal/views/home.py
+++ portal/views/home.py
@@ -403,9 +403,13 @@ def process_newsletter_form(request):
         if newsletter_form.is_valid():
             user_email = newsletter_form.cleaned_data['email']
             add_to_salesforce("", "", user_email)
-            return render(request, 'portal/confirm_news_signup.html')
+            messages.success(request, 'Thank you for signing up!')
+            next = request.POST.get('URL')
+            return HttpResponseRedirect(next)
 
-        return render(request, 'portal/news_signup_fail.html')
+        messages.error(request, 'Invalid email address. Please try again.', extra_tags='sub-nav--warning')
+        next = request.POST.get('URL')
+        return HttpResponseRedirect(next)
 
 
 def home(request):
