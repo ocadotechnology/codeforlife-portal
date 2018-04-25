3654f3b8c089b440cd727343d92a73118f86e8b0
Ria Jha <ria.jha@ocado.com>
2018-04-25 15:39:46 +0100

    Fixed test cases and merge conflict in base

--- portal/tests/test_newsletter_footer.py
+++ portal/tests/test_newsletter_footer.py
@@ -46,11 +46,14 @@ class TestNewsletterFooter(TestCase):
         client = Client()
         data = {'email': 'valid_email@example.com'}
         response = client.post(url, data)
-        self.assertContains(response, "Signup successful!")
+        messages = list(response.wsgi_request._messages)
+        self.assertEquals(1, len([m for m in messages if m.tags == 'success']))
+
 
     def test_newsletter_signup_fail(self):
         url = reverse('process_newsletter_form')
         client = Client()
         data = {'email': 'invalid_email'}
         response = client.post(url, data)
-        self.assertContains(response, "Invalid Email Address")
+        messages = list(response.wsgi_request._messages)
+        self.assertEquals(1, len([m for m in messages if 'error' in m.tags]))
