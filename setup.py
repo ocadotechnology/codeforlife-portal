a95479d46bf1764d17bde68853b344a128fe80f1
Niket Shah <masterniket@gmail.com>
2018-05-18 08:55:45 +0100

    Update django version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='codeforlife-portal',
       packages=find_packages(),
       include_package_data=True,
       install_requires=[
-        'django==1.8.2',
+        'django==1.8.3',
         'django-appconf==1.0.1',
         'django-countries==3.4.1',
         'djangorestframework==3.1.3',
