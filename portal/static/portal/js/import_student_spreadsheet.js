244465d1b1789d05e78448fbf365b7718814e1cd
Sarah Covey <sarah_covey@asl.org>
2018-06-29 13:32:24 +0100

    added blank lines between variables and functions and combined script tags

--- portal/static/portal/js/import_student_spreadsheet.js
+++ portal/static/portal/js/import_student_spreadsheet.js
@@ -1,13 +1,15 @@
+    var fileReader, theText = document.getElementById("form-create-students").elements.item(1);
+
     function activateImport(){
         $("#studentNamesFileImport").click();
     }
 
-    var fileReader, theText = document.getElementById("form-create-students").elements.item(1);
     function writeStudents(){
         fileReader = new FileReader();
         fileReader.readAsText(document.getElementById("studentNamesFileImport").files[0]);
         fileReader.addEventListener("loadend", showNames);
     }
+
     function showNames(){
         names = fileReader.result;
         names = names.split(",").join("\n");
