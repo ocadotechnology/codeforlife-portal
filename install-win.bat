c531b67de993e5cd8a8a6c41ef12fff735d08403
Niket Shah <masterniket@gmail.com>
2018-07-17 14:14:38 +0100

    Remove ruby sass dependency (#772)
    * Remove ruby sass dependency
    
    * Add libsass django-pipeline compiler
    
    * fix pipeline compilers import
    
    * Add new line at end of file

--- install-win.bat
+++ install-win.bat
@@ -1,4 +1,3 @@
 pip install -r requirements.txt
 pip install -r ..\portal\windows-requirements.txt
-pip install -r ..\ocargo\requirements.txt
-gem install sass --version "3.3.4"
+pip install -r ..\rapid-router\requirements.txt
---
+++ portal/pipeline_compilers/libsass_compiler.py
@@ -0,0 +1,30 @@
+"""Libsass compiler for django-pipeline.
+Speedups development and/or production when compiling sass assets. No need of
+ruby sass anymore.
+"""
+
+import sass
+import codecs
+from pipeline.compilers import CompilerBase
+from django.conf import settings
+
+
+class LibSassCompiler(CompilerBase):
+    """Compiler that uses libsass."""
+
+    output_extension = 'css'
+
+    def match_file(self, filename):
+        """Check files extension to use them."""
+        return filename.endswith(('.scss', '.sass'))
+
+    def compile_file(self, infile, outfile, outdated=False, force=False):
+        """Process sass file."""
+        myfile = codecs.open(outfile, 'w', 'utf-8')
+
+        if settings.DEBUG:
+            myfile.write(sass.compile(filename=infile))
+        else:
+            myfile.write(sass.compile(filename=infile,
+                                      output_style='compressed'))
+        return myfile.close()
