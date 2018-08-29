173c4c6a763052db2e9ae5072ada5601ba8d0e00
Ria Jha <ria.jha@ocado.com>
2018-08-29 13:53:33 +0100

    Django-otp

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(name='codeforlife-portal',
           'postcodes==0.1',
           'django-formtools==1.0',
           'django-two-factor-auth==1.5.0',
+          'django-otp==0.4.3',
           'urllib3==1.22',
           'requests==2.18.4',
 
