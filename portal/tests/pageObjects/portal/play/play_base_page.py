613a6d2e6e8d72c66441cde7584e5b11b05646be
faucomte97 <f.aucomte@hotmail.co.uk>
2018-11-26 13:41:20 +0000

    I'm OCD about spacing okay

--- portal/tests/pageObjects/portal/play/play_base_page.py
+++ portal/tests/pageObjects/portal/play/play_base_page.py
@@ -45,8 +45,8 @@ class PlayBasePage(BasePage):
     def logout(self):
         self.open_user_options_box()
         self.browser.find_element_by_id('logout_button').click()
-        from portal.tests.pageObjects.portal.home_page import HomePage
 
+        from portal.tests.pageObjects.portal.home_page import HomePage
         return HomePage(self.browser)
 
     def go_to_account_page(self):
