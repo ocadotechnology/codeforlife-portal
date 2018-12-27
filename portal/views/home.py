da43b4e2cbc1e1b4adfa62d9d5df28a2f21d93ba
faucomte97 <f.aucomte@hotmail.co.uk>
2018-12-27 14:50:07 +0000

    Brought back import to be removed in another PR

--- portal/views/home.py
+++ portal/views/home.py
@@ -34,7 +34,7 @@
 # copyright notice and these terms. You must not misrepresent the origins of this
 # program; modified versions of the program must be marked as such and not
 # identified as the original program.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.contrib import messages as messages
