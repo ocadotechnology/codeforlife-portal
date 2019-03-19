688098edb7467b95d0331deb5a7a4f47c36ff716
Niket Shah <masterniket@gmail.com>
2019-03-19 14:18:49 +0000

    refactor: Make urls consistent in the example project
    Make urls consistent in the example project with the deployed website

---
+++ example_project/example_project/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, patterns, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^', include('portal.urls')),
+    url(r'^administration/', include(admin.site.urls)),
+    url(r'^rapidrouter/', include('game.urls')),
+    url(r'^reports/', include('reports.urls')),
+    url(r'^aimmo/', include('aimmo.urls')),
+)
