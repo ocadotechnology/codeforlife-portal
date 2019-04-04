06f3a9052fbb2fc67df2d4d17aae020267a219e2
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-04-04 16:03:50 +0100

    Optimise middleware

--- deploy/middleware/exceptionlogging.py
+++ deploy/middleware/exceptionlogging.py
@@ -6,16 +6,12 @@ class ExceptionLoggingMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        self.process_exception(request)
-
-        response = self.get_response(request)
-
-        return response
-
-    def process_exception(self, request):
         logging.exception(
             "Exception occurred while handling %s request to %s",
             request.method,
             request.path,
         )
-        return None
+
+        response = self.get_response(request)
+
+        return response
