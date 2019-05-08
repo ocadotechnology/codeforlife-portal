8aaa8f45093426e95775056f09792e4d20c4ac60
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-05-08 15:38:47 +0100

    coverage improvement

--- portal/middleware/online_status/__init__.py
+++ portal/middleware/online_status/__init__.py
@@ -1,12 +1,3 @@
 from .middleware import OnlineStatusMiddleware
 
-VERSION = (0, 1, 1)
-
 __all__ = ["OnlineStatusMiddleware"]
-
-
-def get_version():
-    version = "%s.%s" % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = "%s.%s" % (version, VERSION[2])
-    return version
