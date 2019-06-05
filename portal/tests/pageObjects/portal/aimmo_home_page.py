914d8ac27a730375ea10f272bfb149fe4eca1c36
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-06-05 09:25:12 +0100

    test fix?

--- portal/tests/pageObjects/portal/aimmo_home_page.py
+++ portal/tests/pageObjects/portal/aimmo_home_page.py
@@ -52,7 +52,7 @@ class AimmoHomePage(BasePage):
 
     def click_delete_game_button(self):
         self.browser.find_element_by_link_text("Delete").click()
-        self.browser.find_element_by_link_text("Confirm").click()
+        self.browser.find_element_by_class_name("button--confirm").click()
 
     def click_play_game_button(self):
         self.browser.find_element_by_link_text("Play").click()
