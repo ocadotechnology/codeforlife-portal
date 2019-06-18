1178608e94bb69f290b3a720dc5d0aac9272d1fb
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-06-18 13:58:24 +0100

    changed ajax

--- portal/static/portal/js/delete_aimmo_game.js
+++ portal/static/portal/js/delete_aimmo_game.js
@@ -58,6 +58,9 @@ function deleteGame() {
         type: 'delete',
         headers: {
             "X-CSRFToken": $('input[name=csrfmiddlewaretoken]').val()
+        },
+        xhrFields: {
+            withCredentials: true
         }
     })
     hidePopupConfirmation();
