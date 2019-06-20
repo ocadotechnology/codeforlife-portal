aba0dad034c38f067a694a1c6c7d99175563dd6b
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-06-20 12:55:59 +0100

    refactored test

--- portal/tests/test_preview_users.py
+++ portal/tests/test_preview_users.py
@@ -152,7 +152,7 @@ class SeleniumTestPreviewUsers(TeachBasePage):
 
         self.assertIn("/aimmo/play/1/", self.selenium.driver.current_url)
 
-    def test_preview_user_cannot_create_empty_game(self):
+    def test_preview_user_cannot_create_invalid_game_name(self):
         email, password = signup_teacher_directly_as_preview_user()
         create_organisation_directly(email, True)
         _, _, access_code = create_class_directly(email)
@@ -168,19 +168,7 @@ class SeleniumTestPreviewUsers(TeachBasePage):
 
         self.assertEqual(
             page.get_input_game_name_placeholder(), "Give your new game a name..."
-        )
 
-    def test_preview_user_cannot_create_invalid_game_name(self):
-        email, password = signup_teacher_directly_as_preview_user()
-        create_organisation_directly(email, True)
-        _, _, access_code = create_class_directly(email)
-        create_school_student_directly(access_code)
-
-        self.selenium.get(self.live_server_url)
-        page = HomePage(self.selenium).go_to_login_page().login(email, password)
-        page = page.go_to_aimmo_home_page()
-
-        page.click_create_new_game_button()
         page.input_new_game_name("it's an invalid name")
         page.click_create_game_button()
 
