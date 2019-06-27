c5171b0c5be41eafc2fc0a834a3d2caef208a145
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-06-27 15:21:00 +0100

    fixed url + removed function

--- portal/templatetags/future.py
+++ portal/templatetags/future.py
@@ -2,9 +2,3 @@ from django.template import Library
 from django.template.defaulttags import cycle as cycle_original
 
 register = Library()
-
-
-@register.tag
-def cycle(*args, **kwargs):
-    """ A stub to get SortableTabularInline to work """
-    return cycle_original(*args, **kwargs)
