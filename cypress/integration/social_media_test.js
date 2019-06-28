7db4532f5c9be32838549cb4a9bf299b637b3c1a
Jack Douglas <jack_douglas@asl.org>
2019-06-28 11:42:52 +0100

    Remove test for google+

--- cypress/integration/social_media_test.js
+++ cypress/integration/social_media_test.js
@@ -8,9 +8,4 @@ it ('clicks the link "type"', function (){
 
     cy.get('[src="/static/portal/img/facebook.png"]').click()
 
-    cy.visit('http://localhost:8000')
-
-    cy.get('[src="/static/portal/img/google%2B.png"]').click()
-
-
 })
