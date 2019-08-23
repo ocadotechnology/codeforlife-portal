a43b0f0856e29246e04e9df7647792ba32a6a039
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-08-23 09:26:12 +0100

    added migration tests

--- portal/tests/test_migration_remove_preview_users.py
+++ portal/tests/test_migration_remove_preview_users.py
@@ -34,44 +34,19 @@
 # copyright notice and these terms. You must not misrepresent the origins of this
 # program; modified versions of the program must be marked as such and not
 # identified as the original program.
-
-from django.core.cache import cache
-from django.core.exceptions import FieldDoesNotExist
-from two_factor.utils import default_device
+from base_test_migration import MigrationTestCase
+from portal.utils import field_exists
 
 
-def two_factor_cache_key(user):
-    """Cache key for using_two_factor."""
-    return "using-two-factor-%s" % user.pk
+class TestMigrationPreviewUsers(MigrationTestCase):
 
+    start_migration = "0055_add_preview_user"
+    dest_migration = "0056_remove_preview_user"
 
-def _using_two_factor(user):
-    """Returns whether the user is using 2fa or not."""
-    return default_device(user)
+    def test_preview_user_field_removed(self):
+        model = self.django_application.get_model(self.app_name, "UserProfile")
+        self.assertEquals(field_exists(model, "preview_user"), False)
 
-
-def using_two_factor(user):
-    """Returns whether the user is using 2fa or not (Cached)."""
-    if hasattr(user, "using_two_factor_cache"):
-        # First try local memory, as we call this a lot in one request
-        return user.using_two_factor_cache
-    cache_key = two_factor_cache_key(user)
-    val = cache.get(cache_key)
-    if val is not None:
-        # If local memory failed, but we got it from memcache, set local memory
-        user.using_two_factor_cache = val
-        return val
-    val = bool(_using_two_factor(user))
-
-    # We didn't find it in the cache, so set it there and local memory
-    cache.set(cache_key, val, None)  # Cache forever
-    user.using_two_factor_cache = val
-    return val
-
-
-def field_exists(model, field):
-    try:
-        field = model._meta.get_field(field)
-    except FieldDoesNotExist:
-        return False
-    return True
+    def test_eligible_for_testing_field_added(self):
+        model = self.django_application.get_model(self.app_name, "School")
+        self.assertEquals(field_exists(model, "eligible_for_testing"), False)
