4530ee17fdb53cac26eb6dc89c835896e0b40db2
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-09-19 12:13:11 +0100

    forgot to stage one file

--- portal/templatetags/benefits_tags.py
+++ portal/templatetags/benefits_tags.py
@@ -13,11 +13,20 @@ def benefits(page):
     content = content["benefits"]
 
     return {
-        "title": content["title"],
-        "image1": content["image1"],
-        "text1": content["text1"],
-        "image2": content["image2"],
-        "text2": content["text2"],
-        "image3": content["image3"],
-        "text3": content["text3"]
+        "title": content["main_title"],
+        "image1": content["first"]["image"],
+        "title1": content["first"]["title"],
+        "text1": content["first"]["text"],
+        "button1_text": content["first"]["button"]["text"],
+        "button1_link": content["first"]["button"]["link"],
+        "image2": content["second"]["image"],
+        "title2": content["second"]["title"],
+        "text2": content["second"]["text"],
+        "button2_text": content["second"]["button"]["text"],
+        "button2_link": content["second"]["button"]["link"],
+        "image3": content["third"]["image"],
+        "title3": content["third"]["title"],
+        "text3": content["third"]["text"],
+        "button3_text": content["third"]["button"]["text"],
+        "button3_link": content["third"]["button"]["link"]
     }
