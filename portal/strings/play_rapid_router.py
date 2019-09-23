3cabf53c53d992c9a65b0aac79015c912fac892c
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2019-09-23 15:20:04 +0100

    refactor: Make page blurb into a partial (#1057)
    * refactor: Make page blurb into a partial
    
    * Add new line
    
    * Use python data instead of JSON
    
    * Add test for headline partial
    
    * Make test for generic for headline templatetag

---
+++ portal/strings/play_rapid_router.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+HEADLINE = {
+    "title": "Anyone can code, you can too!",
+    "description": "Whether you’re a teacher or a student, Rapid Router guides you, "
+    "and makes learning to code easy and great fun. Using Blockly you can "
+    "advance through the levels to become an Ocado delivery hero.",
+}
