3cd8bd7856c05327bef62747cf1289ac66de4a60
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-09-23 13:51:40 +0100

    Make test for generic for headline templatetag

--- portal/tests/test_partials.py
+++ portal/tests/test_partials.py
@@ -1,15 +1,23 @@
-from django.core.urlresolvers import reverse
-from django.test import Client
+from django.template import Template, Context
 from django.test import TestCase
-
-from portal.strings.play_rapid_router import HEADLINE
 
 
 class TestPartials(TestCase):
     def test_headline(self):
-        c = Client()
-        url = reverse("play")
-        response = c.get(url)
+        test_headline = {"title": "Test title", "description": "Test description"}
 
-        self.assertContains(response, HEADLINE["title"])
-        self.assertContains(response, HEADLINE["description"])
+        context = Context({"HEADLINE": test_headline})
+        template_to_render = Template(
+            "{% load headline_tags %}" '{% headline headline_name="HEADLINE" %}'
+        )
+        rendered_template = template_to_render.render(context)
+
+        self.assertInHTML(
+            "<h1>{}</h1>".format(test_headline["title"]), rendered_template
+        )
+        self.assertInHTML(
+            '<h4 class="col-sm-6 col-center">{}</h4>'.format(
+                test_headline["description"]
+            ),
+            rendered_template,
+        )
