a787a75a901245f45911ce2a0113926941b338a1
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-10-03 18:52:33 +0100

    Fix some CodeClimate issues

--- portal/templatetags/banner_tags.py
+++ portal/templatetags/banner_tags.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.inclusion_tag("portal/partials/banner.html", takes_context=True)
 def banner(context, banner_name):
     banner_data = context[banner_name]
-    
+
     return {
         "title": banner_data["title"],
         "image_class": banner_data["image_class"],
