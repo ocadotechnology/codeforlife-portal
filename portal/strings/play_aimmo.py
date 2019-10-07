cf4fbe5acaece366e5761fc4667f17798f29185b
Maria Mafalda Fernando <30693435+mariaFernando@users.noreply.github.com>
2019-10-07 10:26:36 +0100

    feat: new Kurono landing page (#1060)
    * called play_aimmo.html play_aimmo_old.html and added page w/ partials
    
    * Merge branch 'master' into kurono_landing_page
    
    * url fix
    
    * black formatter
    
    * added videos
    
    * benefits changes
    
    * mobile screen fix, top menu not showing
    
    * fixed mobile
    
    * deleted old html
    
    * some minor fixes
    
    * code climate fixes
    
    * code climate fix
    
    * minor variable changes
    
    * Revert "deleted old html"
    
    This reverts commit e33c2088246f60c0f59f9182287cbc32cb1ff229.
    
    * typo fix and centred challenges
    
    * renamed old html file and increased video height
    
    * minified kurono landing hero jpeg
    
    * changed shadow colours
    
    * renamed level
    
    * changed videos with newer versions
    
    * version cached play aimmo page
    
    * left-align benefits text

---
+++ portal/strings/play_aimmo.py
@@ -0,0 +1,42 @@
+# coding=utf-8
+HEADLINE = {
+    "title": "Join a coding adventure!",
+    "description": "Whether you’re a teacher or a student, Kurono makes learning to code easy and great fun. Using Python, you can travel through time to collect all the museum artifacts.",
+}
+
+CHALLENGES = {
+    "title": "Challenges",
+    "description": "Individual and collaborative challenges keep students engaged and having fun. "
+    "(Coming soon)",
+}
+
+TEACHING_RESOURCES = {
+    "title": "Teaching Resources",
+    "description": "Integrate Kurono in your classroom with our printable student challenges, teacher guidance sheets and supporting materials.",
+}
+
+PLAY_ONLINE = {
+    "title": "Play Online",
+    "description": "No need to install anything, so you can get started right away.",
+}
+
+BENEFITS = {
+    "first": {
+        "image": "portal/img/icon_laptop.png",
+        "title": "Aligned to the curriculum",
+        "text": "Kurono is aligned to the National Computing Curriculum.",
+        "button": {"text": "", "link": ""},
+    },
+    "second": {
+        "image": "portal/img/icon_free.png",
+        "title": "Completely free to use",
+        "text": "We are also open source!",
+        "button": {"text": "", "link": ""},
+    },
+    "third": {
+        "image": "portal/img/icon_multiplayer.svg",
+        "title": "Online multiplayer game",
+        "text": "Classes play together in the same world.",
+        "button": {"text": "", "link": ""},
+    },
+}
