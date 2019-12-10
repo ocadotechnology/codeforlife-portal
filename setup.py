b156e523724a0046c6d35fa0955b9d548af20bfc
Maria Mafalda Fernando <maria.fernando@ocado.com>
2019-12-10 13:58:28 +0000

    upgraded phonenumbers to 8.11.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         "sqlparse",
         "libsass",
         "django-forms-bootstrap",
-        "phonenumbers==7.7.5",
+        "phonenumbers==8.11.0",
         "future"
     ],
     zip_safe=False,
