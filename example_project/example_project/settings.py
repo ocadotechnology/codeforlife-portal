77d4e143a7155505b0654bc8254aa1692a0a717e
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-12-30 10:43:42 +0000

    Remove Python 2.7 stage in travis and add classifiers in setup.py

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -46,6 +46,8 @@ TEMPLATES = [
     }
 ]
 
+SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -73,7 +75,7 @@ LOGIN_REDIRECT_URL = "/teach/dashboard/"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-INSTALLED_APPS = ("portal", "captcha", "django_forms_bootstrap")
+INSTALLED_APPS = ("portal", "captcha")
 
 PIPELINE_ENABLED = False
 
