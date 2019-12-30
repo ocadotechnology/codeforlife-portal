0126f9b1e29a1113b3978911f079df266a40bfc9
Florian Aucomte <f.aucomte@hotmail.co.uk>
2019-12-30 16:30:50 +0000

    Updated Chromedriver + selenium version

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -46,8 +46,6 @@ TEMPLATES = [
     }
 ]
 
-SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
