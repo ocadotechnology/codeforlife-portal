001fef2b74d1d2fb5f2bd25749f41165adee3358
Maria Mafalda Fernando <maria.fernando@ocado.com>
2020-01-02 13:01:10 +0000

    changed copies with no links

--- portal/strings/play.py
+++ portal/strings/play.py
@@ -11,7 +11,8 @@ PLAY_HEADLINE = {
     "title": "Anyone can code, you can too!",
     "description": "Whether you’re a teacher or a student, our games support and "
     "guide you, making learning to code great fun. Get started with Rapid Router "
-    "designed for students new to coding. Advance to Kurono where you can test "
+    "designed for students new to coding. This is where you will build up your "
+    "ability until you are ready to advance to Kurono, where you can test "
     "your skills in Python.",
 }
 
@@ -42,7 +43,7 @@ RAPID_ROUTER_BANNER = {
     "title": "Rapid Router",
     "description": "Use Blockly (and later Python) to help Dee drive the van "
     "to make all the deliveries and become an Ocado delivery hero.",
-    "button_text": "Play Now",
+    "button_text": "Find out more",
     "button_link": "play_rapid_router",
     "ages": "Ages 6—13",
     "background_image_class": "banner--rapid-router--background",
@@ -53,7 +54,7 @@ KURONO_BANNER = {
     "description": "Using intermediate Python, join your class on a time "
     "travelling adventure. Work together to complete challenges and unlock "
     "new time periods.",
-    "button_text": "Try Now",
+    "button_text": "Find out more",
     "button_link": "play_aimmo",
     "ages": "Ages 13+",
     "background_image_class": "banner--aimmo--background",
