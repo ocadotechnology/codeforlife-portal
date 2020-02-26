c90c0eb882957eee8a9c949fdc2362206aa29dbf
Niket Shah <masterniket@gmail.com>
2020-02-26 10:11:20 +0000

    run black
    Signed-off-by: Niket Shah <masterniket@gmail.com>

--- portal/app_settings.py
+++ portal/app_settings.py
@@ -59,4 +59,4 @@ DOTMAILER_USER = getattr(settings, "DOTMAILER_USER", "")
 DOTMAILER_PASSWORD = getattr(settings, "DOTMAILER_PASSWORD", "")
 
 #: Dotmailer default preferences to what users are signed up to
-DOTMAILER_DEFAULT_PREFERENCES =getattr(settings, "DOTMAILER_DEFAULT_PREFERENCES", [])
+DOTMAILER_DEFAULT_PREFERENCES = getattr(settings, "DOTMAILER_DEFAULT_PREFERENCES", [])
