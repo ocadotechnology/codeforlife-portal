f80bcb594ab651a196890745968f8bc3dac2655b
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2020-04-14 14:46:00 +0100

    fix: Improve admin pages (#1200)
    * Replace problematic filter bars with column in table
    
    * Merge branch 'master' into improve_admin_pages
    
    * Merge branch 'master' into improve_admin_pages
    
    * Remove school filters and add search functionality
    
    * Merge branch 'master' into improve_admin_pages
    
    * Merge branch 'improve_admin_pages' of https://github.com/ocadotechnology/codeforlife-portal into improve_admin_pages
    
    * Add school search to Teacher page

--- portal/admin.py
+++ portal/admin.py
@@ -54,9 +54,13 @@ class ClassAdmin(admin.ModelAdmin):
         "name",
         "teacher__new_user__first_name",
         "teacher__new_user__last_name",
+        "teacher__school__name",
     ]
-    list_filter = ["teacher", "teacher__school"]
-    readonly_fields = ["teacher"]
+    list_display = ["__str__", "teacher", "teacher_school"]
+    raw_id_fields = ["teacher"]
+
+    def teacher_school(self, obj):
+        return obj.teacher.school
 
 
 class SchoolAdmin(admin.ModelAdmin):
@@ -65,19 +69,39 @@ class SchoolAdmin(admin.ModelAdmin):
 
 
 class StudentAdmin(admin.ModelAdmin):
-    search_fields = ["new_user__first_name", "new_user__last_name"]
-    list_filter = [
+    search_fields = [
+        "new_user__first_name",
+        "new_user__last_name",
+        "class_field__name",
+        "class_field__teacher__new_user__first_name",
+        "class_field__teacher__new_user__last_name",
+        "class_field__teacher__school__name",
+    ]
+    list_display = [
+        "__str__",
         "class_field",
-        "class_field__teacher",
-        "class_field__teacher__school",
+        "class_field_teacher",
+        "class_field_school",
     ]
     readonly_fields = ["user", "new_user"]
     raw_id_fields = ["class_field", "pending_class_request"]
 
+    def class_field_teacher(self, obj):
+        if obj.class_field:
+            return obj.class_field.teacher
+        else:
+            return "Independent"
+
+    def class_field_school(self, obj):
+        if obj.class_field:
+            return obj.class_field.teacher.school
+        else:
+            return "Independent"
+
 
 class TeacherAdmin(admin.ModelAdmin):
-    search_fields = ["new_user__first_name", "new_user__last_name"]
-    list_filter = ["school"]
+    search_fields = ["new_user__first_name", "new_user__last_name", "school__name"]
+    list_display = ["__str__", "school"]
     readonly_fields = ["user", "new_user"]
     raw_id_fields = ["school", "pending_join_request"]
 
@@ -101,6 +125,7 @@ class EmailVerificationAdmin(admin.ModelAdmin):
         "user__username",
         "user__date_joined",
     ]
+    readonly_fields = ["user", "token"]
 
 
 UserAdmin.list_display += ("date_joined",)
