4b42f2dc82f3646528a962442648f5f2f017adfa
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-04-14 12:00:12 +0100

    Remove school filters and add search functionality

--- portal/admin.py
+++ portal/admin.py
@@ -54,8 +54,8 @@ class ClassAdmin(admin.ModelAdmin):
         "name",
         "teacher__new_user__first_name",
         "teacher__new_user__last_name",
+        "teacher__school__name",
     ]
-    list_filter = ["teacher__school"]
     list_display = ["__str__", "teacher", "teacher_school"]
     raw_id_fields = ["teacher"]
 
@@ -69,15 +69,32 @@ class SchoolAdmin(admin.ModelAdmin):
 
 
 class StudentAdmin(admin.ModelAdmin):
-    search_fields = ["new_user__first_name", "new_user__last_name"]
-    list_filter = ["class_field__teacher__school"]
-    list_display = ["__str__", "class_field", "class_field_teacher"]
+    search_fields = [
+        "new_user__first_name",
+        "new_user__last_name",
+        "class_field__name",
+        "class_field__teacher__new_user__first_name",
+        "class_field__teacher__new_user__last_name",
+        "class_field__teacher__school__name",
+    ]
+    list_display = [
+        "__str__",
+        "class_field",
+        "class_field_teacher",
+        "class_field_school",
+    ]
     readonly_fields = ["user", "new_user"]
     raw_id_fields = ["class_field", "pending_class_request"]
 
     def class_field_teacher(self, obj):
         if obj.class_field:
             return obj.class_field.teacher
+        else:
+            return "Independent"
+
+    def class_field_school(self, obj):
+        if obj.class_field:
+            return obj.class_field.teacher.school
         else:
             return "Independent"
 
@@ -108,7 +125,7 @@ class EmailVerificationAdmin(admin.ModelAdmin):
         "user__username",
         "user__date_joined",
     ]
-    readonly_fields = ["user", "token", "expiry"]
+    readonly_fields = ["user", "token"]
 
 
 UserAdmin.list_display += ("date_joined",)
