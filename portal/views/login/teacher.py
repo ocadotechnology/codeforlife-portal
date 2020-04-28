ced689f2d430233bb44a7b5547e62f1327364591
Niket <niket.shah1@ocado.com>
2020-04-28 13:49:50 +0100

    redirect teachers to dashboard if they try to login again
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- portal/views/login/teacher.py
+++ portal/views/login/teacher.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.views import LoginView
 from portal.forms.teach import TeacherLoginForm
+from django.core.urlresolvers import reverse_lazy
 
 
 class TeacherLoginView(LoginView):
     authentication_form = TeacherLoginForm
     template_name = "portal/login/teacher.html"
     success_url = "portal/email_verification_needed.html"
-
+    redirect_authenticated_user = reverse_lazy("dashboard")
