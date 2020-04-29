b3ccfcd39e367785edf700ef3640904b9f5abe6c
Niket <niket.shah1@ocado.com>
2020-04-29 08:48:00 +0100

    Use two factor form for teacher login
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- portal/views/login/teacher.py
+++ portal/views/login/teacher.py
@@ -1,10 +1,31 @@
-from django.contrib.auth.views import LoginView
+from two_factor.views import LoginView
+from two_factor.forms import AuthenticationTokenForm, BackupTokenForm
 from portal.forms.teach import TeacherLoginForm
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect
+from portal.utils import using_two_factor
 
 
 class TeacherLoginView(LoginView):
-    authentication_form = TeacherLoginForm
+    TEMPLATES = {
+        "auth": "portal/login/teacher.html",
+        "token": "two_factor/core/login.html",
+        "backup": "two_factor/core/login.html",
+    }
+    form_list = (
+        ("auth", TeacherLoginForm),
+        ("token", AuthenticationTokenForm),
+        ("backup", BackupTokenForm),
+    )
     template_name = "portal/login/teacher.html"
-    success_url = "portal/email_verification_needed.html"
+    success_url = reverse_lazy("dashboard")
     redirect_authenticated_user = reverse_lazy("dashboard")
+
+    def get_template_names(self):
+        return [self.TEMPLATES[self.steps.current]]
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect(self.redirect_authenticated_user)
+        return super(TeacherLoginView, self).get(request, *args, **kwargs)
