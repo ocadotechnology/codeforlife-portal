297bf425832526795832811cb0929a0c640db80c
Niket <niket.shah1@ocado.com>
2020-05-04 13:04:47 +0100

    Add student message back in after they log in
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- portal/views/login/student.py
+++ portal/views/login/student.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth.views import LoginView
 from django.core.urlresolvers import reverse_lazy
+from django.utils.html import escape
 
 from portal.forms.play import StudentLoginForm
 
@@ -10,6 +12,29 @@ class StudentLoginView(LoginView):
     success_url = reverse_lazy("student_details")
     redirect_authenticated_user = reverse_lazy("student_details")
 
+    def _add_logged_in_as_message(self, request):
+        student = request.user.userprofile.student
+        student_class = student.class_field
+        student_school = student_class.teacher.school
+
+        messages.info(
+            request,
+            (
+                "You are logged in as a member of class: <strong>"
+                + escape(student_class.name)
+                + "</strong>, in school or club: <strong>"
+                + escape(student_school.name)
+                + "</strong>."
+            ),
+            extra_tags="safe",
+        )
+
     def get_success_url(self):
-        url = self.get_redirect_url()
-        return url or self.success_url
+        redirect_url = self.get_redirect_url()
+
+        if redirect_url:
+            return redirect_url
+
+        self._add_logged_in_as_message(self.request)
+
+        return self.success_url
