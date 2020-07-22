268f3ab719c2923cc7e76f32648374722e853e6b
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-07-22 14:01:49 +0100

    Rename migrations

--- portal/migrations/0058_move_to_common_models.py
+++ portal/migrations/0058_move_to_common_models.py
@@ -9,7 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('game', '0071_auto_20200716_1728'),
+        ('game', '0071_use_common_models'),
         ('portal', '0057_delete_frontpagenews'),
     ]
 
