7723d64ba254b23cafe998e49c45dd39c6c16108
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-08-14 19:01:00 +0100

    fix: Add install_requires to common package

--- cfl_common/setup.py
+++ cfl_common/setup.py
@@ -13,6 +13,10 @@ setup(
     packages=find_packages(),
     version=version,
     include_package_data=True,
+    install_requires=[
+        "django>=1.10.8, <= 1.11.24",
+        "django-countries==5.4",
+    ],
     tests_require=[],
     test_suite="tests",
     classifiers=[
