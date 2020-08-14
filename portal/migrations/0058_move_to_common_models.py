aa18e5faa43df170d3be2e7c4fad4675cc3749f9
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-08-14 12:27:55 +0100

    Add migration test for portal

--- portal/migrations/0058_move_to_common_models.py
+++ portal/migrations/0058_move_to_common_models.py
@@ -9,6 +9,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
+        ('common', '0001_initial'),
         ('game', '0071_use_common_models'),
         ('portal', '0057_delete_frontpagenews'),
     ]
