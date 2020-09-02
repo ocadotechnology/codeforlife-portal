c1905ad06c96a7f056745fc04e9ea7a5d7673176
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-09-02 16:55:40 +0100

    Create hero card inclusion tag

---
+++ portal/templatetags/hero_card_tags.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag("portal/partials/hero_card.html", takes_context=True)
+def hero_card(context):
+    return {
+        "image1": context["HERO_CARD"]["image"],
+        "heading": context["HERO_CARD"]["heading"],
+        "description": context["HERO_CARD"]["description"],
+        "button1_text": context["HERO_CARD"]["button1_text"],
+        "button2_text": context["HERO_CARD"]["button2_text"],
+    }
