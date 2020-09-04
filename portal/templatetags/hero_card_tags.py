baf45276a3736216e7ca397d5a18e54e445f9223
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-09-04 11:15:04 +0100

    Update card design

--- portal/templatetags/hero_card_tags.py
+++ portal/templatetags/hero_card_tags.py
@@ -5,12 +5,4 @@ register = template.Library()
 
 @register.inclusion_tag("portal/partials/hero_card.html", takes_context=True)
 def hero_card(context):
-    return {
-        "image": context["HERO_CARD"]["image"],
-        "title": context["HERO_CARD"]["title"],
-        "description": context["HERO_CARD"]["description"],
-        "button1_text": context["HERO_CARD"]["button1_text"],
-        "button1_link": context["HERO_CARD"]["button1_link"],
-        "button2_text": context["HERO_CARD"]["button2_text"],
-        "button2_link": context["HERO_CARD"]["button2_link"],
-    }
+    return context["HERO_CARD"]
