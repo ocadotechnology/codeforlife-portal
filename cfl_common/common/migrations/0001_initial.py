6949ff623b938c5f978090435a936026c7eba501
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2020-09-30 16:02:26 +0100

    feat: Add worksheet fixture (#1271)
    * feat: Add worksheet fixture
    
    * Add worksheets and update characters
    
    * Install latest aimmo beta
    
    * Update snapshot
    
    * Run Black
    
    * Rename test
    
    * Unregister characters from admin
    
    * Rename test again

--- cfl_common/common/migrations/0001_initial.py
+++ cfl_common/common/migrations/0001_initial.py
@@ -20,18 +20,36 @@ class Migration(migrations.Migration):
         migrations.SeparateDatabaseAndState(
             state_operations=[
                 migrations.CreateModel(
-                    name='Class',
+                    name="Class",
                     fields=[
-                        ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                        ('name', models.CharField(max_length=200)),
-                        ('access_code', models.CharField(max_length=5)),
-                        ('classmates_data_viewable', models.BooleanField(default=False)),
-                        ('always_accept_requests', models.BooleanField(default=False)),
-                        ('accept_requests_until', models.DateTimeField(null=True)),
-                        ('teacher', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='class_teacher', to='common.Teacher'))
+                        (
+                            "id",
+                            models.AutoField(
+                                auto_created=True,
+                                primary_key=True,
+                                serialize=False,
+                                verbose_name="ID",
+                            ),
+                        ),
+                        ("name", models.CharField(max_length=200)),
+                        ("access_code", models.CharField(max_length=5)),
+                        (
+                            "classmates_data_viewable",
+                            models.BooleanField(default=False),
+                        ),
+                        ("always_accept_requests", models.BooleanField(default=False)),
+                        ("accept_requests_until", models.DateTimeField(null=True)),
+                        (
+                            "teacher",
+                            models.ForeignKey(
+                                on_delete=django.db.models.deletion.CASCADE,
+                                related_name="class_teacher",
+                                to="common.Teacher",
+                            ),
+                        ),
                     ],
                     options={
-                        'verbose_name_plural': 'classes',
+                        "verbose_name_plural": "classes",
                     },
                 ),
             ],
@@ -40,18 +58,35 @@ class Migration(migrations.Migration):
         migrations.SeparateDatabaseAndState(
             state_operations=[
                 migrations.CreateModel(
-                    name='School',
+                    name="School",
                     fields=[
-                        ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                        ('name', models.CharField(max_length=200)),
-                        ('postcode', models.CharField(max_length=10)),
-                        ('town', models.CharField(max_length=200)),
-                        ('latitude', models.CharField(max_length=20)),
-                        ('longitude', models.CharField(max_length=20)),
-                        ('country', django_countries.fields.CountryField(max_length=2)),
+                        (
+                            "id",
+                            models.AutoField(
+                                auto_created=True,
+                                primary_key=True,
+                                serialize=False,
+                                verbose_name="ID",
+                            ),
+                        ),
+                        ("name", models.CharField(max_length=200)),
+                        ("postcode", models.CharField(max_length=10)),
+                        ("town", models.CharField(max_length=200)),
+                        ("latitude", models.CharField(max_length=20)),
+                        ("longitude", models.CharField(max_length=20)),
+                        ("country", django_countries.fields.CountryField(max_length=2)),
                     ],
                     options={
-                        'permissions': (('view_aggregated_data', 'Can see available aggregated data'), ('view_map_data', "Can see schools' location displayed on map")),
+                        "permissions": (
+                            (
+                                "view_aggregated_data",
+                                "Can see available aggregated data",
+                            ),
+                            (
+                                "view_map_data",
+                                "Can see schools' location displayed on map",
+                            ),
+                        ),
                     },
                 ),
             ],
@@ -60,13 +95,52 @@ class Migration(migrations.Migration):
         migrations.SeparateDatabaseAndState(
             state_operations=[
                 migrations.CreateModel(
-                    name='Student',
+                    name="Student",
                     fields=[
-                        ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                        ('class_field', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='students', to='common.Class')),
-                        ('new_user', models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='new_student', to=settings.AUTH_USER_MODEL)),
-                        ('pending_class_request', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='class_request', to='common.Class')),
-                        ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='common.UserProfile'))
+                        (
+                            "id",
+                            models.AutoField(
+                                auto_created=True,
+                                primary_key=True,
+                                serialize=False,
+                                verbose_name="ID",
+                            ),
+                        ),
+                        (
+                            "class_field",
+                            models.ForeignKey(
+                                null=True,
+                                on_delete=django.db.models.deletion.CASCADE,
+                                related_name="students",
+                                to="common.Class",
+                            ),
+                        ),
+                        (
+                            "new_user",
+                            models.OneToOneField(
+                                blank=True,
+                                null=True,
+                                on_delete=django.db.models.deletion.CASCADE,
+                                related_name="new_student",
+                                to=settings.AUTH_USER_MODEL,
+                            ),
+                        ),
+                        (
+                            "pending_class_request",
+                            models.ForeignKey(
+                                null=True,
+                                on_delete=django.db.models.deletion.CASCADE,
+                                related_name="class_request",
+                                to="common.Class",
+                            ),
+                        ),
+                        (
+                            "user",
+                            models.OneToOneField(
+                                on_delete=django.db.models.deletion.CASCADE,
+                                to="common.UserProfile",
+                            ),
+                        ),
                     ],
                 ),
             ],
@@ -75,15 +149,55 @@ class Migration(migrations.Migration):
         migrations.SeparateDatabaseAndState(
             state_operations=[
                 migrations.CreateModel(
-                    name='Teacher',
+                    name="Teacher",
                     fields=[
-                        ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                        ('title', models.CharField(max_length=35)),
-                        ('is_admin', models.BooleanField(default=False)),
-                        ('new_user', models.OneToOneField(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='new_teacher', to=settings.AUTH_USER_MODEL)),
-                        ('pending_join_request', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='join_request', to='common.School')),
-                        ('school', models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='teacher_school', to='common.School')),
-                        ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='common.UserProfile'))
+                        (
+                            "id",
+                            models.AutoField(
+                                auto_created=True,
+                                primary_key=True,
+                                serialize=False,
+                                verbose_name="ID",
+                            ),
+                        ),
+                        ("title", models.CharField(max_length=35)),
+                        ("is_admin", models.BooleanField(default=False)),
+                        (
+                            "new_user",
+                            models.OneToOneField(
+                                blank=True,
+                                null=True,
+                                on_delete=django.db.models.deletion.CASCADE,
+                                related_name="new_teacher",
+                                to=settings.AUTH_USER_MODEL,
+                            ),
+                        ),
+                        (
+                            "pending_join_request",
+                            models.ForeignKey(
+                                blank=True,
+                                null=True,
+                                on_delete=django.db.models.deletion.CASCADE,
+                                related_name="join_request",
+                                to="common.School",
+                            ),
+                        ),
+                        (
+                            "school",
+                            models.ForeignKey(
+                                null=True,
+                                on_delete=django.db.models.deletion.CASCADE,
+                                related_name="teacher_school",
+                                to="common.School",
+                            ),
+                        ),
+                        (
+                            "user",
+                            models.OneToOneField(
+                                on_delete=django.db.models.deletion.CASCADE,
+                                to="common.UserProfile",
+                            ),
+                        ),
                     ],
                 ),
             ],
@@ -92,13 +206,33 @@ class Migration(migrations.Migration):
         migrations.SeparateDatabaseAndState(
             state_operations=[
                 migrations.CreateModel(
-                    name='UserProfile',
+                    name="UserProfile",
                     fields=[
-                        ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                        ('can_view_aggregated_data', models.BooleanField(default=False)),
-                        ('developer', models.BooleanField(default=False)),
-                        ('awaiting_email_verification', models.BooleanField(default=False)),
-                        ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+                        (
+                            "id",
+                            models.AutoField(
+                                auto_created=True,
+                                primary_key=True,
+                                serialize=False,
+                                verbose_name="ID",
+                            ),
+                        ),
+                        (
+                            "can_view_aggregated_data",
+                            models.BooleanField(default=False),
+                        ),
+                        ("developer", models.BooleanField(default=False)),
+                        (
+                            "awaiting_email_verification",
+                            models.BooleanField(default=False),
+                        ),
+                        (
+                            "user",
+                            models.OneToOneField(
+                                on_delete=django.db.models.deletion.CASCADE,
+                                to=settings.AUTH_USER_MODEL,
+                            ),
+                        ),
                     ],
                 ),
             ],
--- cfl_common/common/migrations/0005_add_worksheets.py
+++ cfl_common/common/migrations/0005_add_worksheets.py
@@ -4,14 +4,16 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
-    dependencies = [("common", "0003_aimmocharacter")]
+    dependencies = [
+        ("common", "0004_add_aimmocharacters"),
+        ("aimmo", "0020_add_info_to_worksheet"),
+    ]
 
     def dummy_reverse_code(app, schema_editor):
         pass
 
     operations = [
         migrations.RunPython(
-            load_data_from_file("aimmo_characters.json"),
-            reverse_code=dummy_reverse_code,
+            load_data_from_file("worksheets.json"), reverse_code=dummy_reverse_code
         )
     ]
