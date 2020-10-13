8f7695f4a8baaabc9e5f193de679bec67336640b
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-10-13 16:28:11 +0100

    Use more fixtures + named tuples

--- portal/tests/test_aimmo_dashboards.py
+++ portal/tests/test_aimmo_dashboards.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
+
 import pytest
+from aimmo.models import Game
+from common.models import Class
 from common.tests.utils.classes import create_class_directly
 from common.tests.utils.student import (
     create_school_student_directly,
@@ -12,50 +16,68 @@ from portal.tests.utils.organisation import create_organisation_directly
 from .utils.aimmo_games import create_aimmo_game_directly
 from .utils.worksheets import create_worksheet_directly
 
+SchoolStudent = namedtuple("student", ["username", "password"])
+IndependentStudent = namedtuple("independent_student", ["username", "password"])
+
 
 @pytest.fixture
-def class_for_student():
+def teacher1_email(db) -> str:
     teacher_email, _ = signup_teacher_directly()
-    create_organisation_directly(teacher_email)
-    klass, _, access_code = create_class_directly(teacher_email)
-    name, password, _ = create_school_student_directly(access_code)
+    return teacher_email
+
+
+@pytest.fixture
+def class1(db, teacher1_email) -> Class:
+    create_organisation_directly(teacher1_email)
+    klass, _, _ = create_class_directly(teacher1_email)
+    return klass
+
+
+@pytest.fixture
+def student1(db, class1) -> SchoolStudent:
+    username, password, _ = create_school_student_directly(class1.access_code)
+    return SchoolStudent(username, password)
+
+
+@pytest.fixture
+def independent_student1(db) -> IndependentStudent:
+    username, password, _ = create_independent_student_directly()
+    return IndependentStudent(username, password)
+
+
+@pytest.fixture
+def aimmo_game1(db, class1) -> Game:
     worksheet = create_worksheet_directly()
     worksheet.student_pdf_name = "TestPDFName"
     worksheet.save()
-    game = create_aimmo_game_directly(klass, worksheet)
-
-    return {
-        "student_details": {
-            "name": name,
-            "password": password,
-            "access_code": access_code,
-        },
-        "game": game
-    }
+    return create_aimmo_game_directly(class1, worksheet)
 
 
 @pytest.mark.django_db
-def test_student_cannot_access_teacher_dashboard():
+def test_student_cannot_access_teacher_dashboard(
+    student1: SchoolStudent, class1: Class
+):
     """
     Given you are logged in as a student,
     When you try to access the teacher dashboard,
     Then you cannot access it and are instead redirected.
     """
-    email, _ = signup_teacher_directly()
-    create_organisation_directly(email)
-    _, _, access_code = create_class_directly(email)
-    name, password, _ = create_school_student_directly(access_code)
-
     c = Client()
     url = reverse("student_login")
     data = {
-        "username": name,
-        "password": password,
-        "access_code": access_code,
+        "username": student1.username,
+        "password": student1.password,
+        "access_code": class1.access_code,
         "g-recaptcha-response": "something",
     }
 
     c.post(url, data)
+
+    student_dashboard_url = reverse("student_aimmo_dashboard")
+
+    response_s = c.get(student_dashboard_url)
+
+    assert response_s.status_code == 200
 
     teacher_dashboard_url = reverse("teacher_aimmo_dashboard")
 
@@ -65,19 +87,19 @@ def test_student_cannot_access_teacher_dashboard():
 
 
 @pytest.mark.django_db
-def test_indep_student_cannot_access_dashboard():
+def test_indep_student_cannot_access_dashboard(
+    independent_student1: IndependentStudent,
+):
     """
     Given you are logged in as an independent student,
     When you try to access the student dashboard,
     Then you can access it but the context only has the banner.
     """
-    username, password, student = create_independent_student_directly()
-
     c = Client()
     url = reverse("independent_student_login")
     data = {
-        "username": username,
-        "password": password,
+        "username": independent_student1.username,
+        "password": independent_student1.password,
         "g-recaptcha-response": "something",
     }
 
@@ -94,7 +116,9 @@ def test_indep_student_cannot_access_dashboard():
 
 
 @pytest.mark.django_db
-def test_student_aimmo_dashboard_loads(class_for_student):
+def test_student_aimmo_dashboard_loads(
+    student1: SchoolStudent, class1: Class, aimmo_game1: Game
+):
     """
     Given an aimmo game is linked to a class,
     When a student of that class goes on the Student Kurono Dashboard page,
@@ -109,9 +133,9 @@ def test_student_aimmo_dashboard_loads(class_for_student):
     c = Client()
     student_login_url = reverse("student_login")
     data = {
-        "username": class_for_student["student_details"]["name"],
-        "password": class_for_student["student_details"]["password"],
-        "access_code": class_for_student["student_details"]["access_code"],
+        "username": student1.username,
+        "password": student1.password,
+        "access_code": class1.access_code,
         "g-recaptcha-response": "something",
     }
 
@@ -124,7 +148,7 @@ def test_student_aimmo_dashboard_loads(class_for_student):
     assert "HERO_CARD" in response.context
     assert "CARD_LIST" in response.context
 
-    class_for_student["game"].delete()
+    aimmo_game1.delete()
 
     url = reverse("student_aimmo_dashboard")
     response = c.get(url)
