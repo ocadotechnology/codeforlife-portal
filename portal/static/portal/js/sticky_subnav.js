cae17dca2abe56c0c9c91743ea0c08a29dcd0eea
Niket <niket.shah1@ocado.com>
2020-10-21 13:04:02 +0100

    Update to jquery 3.5.1
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- portal/static/portal/js/sticky_subnav.js
+++ portal/static/portal/js/sticky_subnav.js
@@ -28,30 +28,31 @@
  identified as the original program.
  */
 
-function toggleStickySubnav(scrollToTop) {
-    $(window).scroll(function() {
-        var currentScroll = $(window).scrollTop();
-        if (currentScroll >= scrollToTop) {
-            if (!$('.sticky-subnav').hasClass("sub-nav--fixed")) {
-                $('.sticky-subnav').addClass('sub-nav--fixed');
-            }
-            if (!$('#sticky-warning').hasClass('sub-nav--warning--fixed')) {
-                $('#sticky-warning').addClass('sub-nav--warning--fixed');
-            }
-            $('#top').addClass('sub-nav--filler');
-        }
-        else {
-            $('#sticky-warning').removeClass('sub-nav--warning--fixed');
-            $('.sticky-subnav').removeClass('sub-nav--fixed');
-            $('#top').removeClass('sub-nav--filler');
-            $('.menu').removeClass('hide');
-        }
-    });
+function toggleStickySubnav (scrollToTop) {
+  $(window).on('scroll', function () {
+    var currentScroll = $(window).scrollTop()
+    if (currentScroll >= scrollToTop) {
+      if (!$('.sticky-subnav').hasClass('sub-nav--fixed')) {
+        $('.sticky-subnav').addClass('sub-nav--fixed')
+      }
+      if (!$('#sticky-warning').hasClass('sub-nav--warning--fixed')) {
+        $('#sticky-warning').addClass('sub-nav--warning--fixed')
+      }
+      $('#top').addClass('sub-nav--filler')
+    } else {
+      $('#sticky-warning').removeClass('sub-nav--warning--fixed')
+      $('.sticky-subnav').removeClass('sub-nav--fixed')
+      $('#top').removeClass('sub-nav--filler')
+      $('.menu').removeClass('hide')
+    }
+  })
 }
 
-$(function() {
-    $("a.x-icon").click(function() {
-        $(this).parent().remove();
-        return false;
-    });
-});
+$(function () {
+  $('a.x-icon').on('click', function () {
+    $(this)
+      .parent()
+      .remove()
+    return false
+  })
+})
