d95ac7acdc3502dd8f7c9884d6a58e5d7bd3ab50
Niket Shah <niket.shah1@ocado.com>
2020-10-22 10:34:49 +0100

    fix: pass in kwargs in materials viewer redirect  (#1288)
    * fix redirect bug for materials viewer
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * add test for bug
    
    Signed-off-by: Niket <niket.shah1@ocado.com>
    
    * removed shared state in tests
    
    Signed-off-by: Niket <niket.shah1@ocado.com>

--- portal/views/teacher/__init__.py
+++ portal/views/teacher/__init__.py
@@ -3,4 +3,4 @@ from django.core.urlresolvers import reverse_lazy
 
 
 def materials_viewer_redirect(request, pdf_name):
-    return redirect(reverse_lazy("materials_viewer", args=pdf_name))
+    return redirect(reverse_lazy("materials_viewer", kwargs={"pdf_name": pdf_name}))
