a634f9404f5838ef0f638d0493ef8fab208e8cc2
Florian Aucomte <f.aucomte@hotmail.co.uk>
2020-11-05 13:04:02 +0000

    Add test for on_delete

---
+++ cfl_common/common/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.apps import apps
+from django.db import models
+from django.test import TestCase
+
+
+class TestModels(TestCase):
+    def test_models_on_delete(self):
+        portal_models = apps.get_app_config("common").get_models()
+
+        for model in portal_models:
+            remote_fields = self._get_model_remote_fields(model)
+
+            for field in remote_fields:
+                if model.__name__ == "Teacher" and (
+                    field.name == "school" or field.name == "pending_join_request"
+                ):
+                    assert field.remote_field.on_delete == models.SET_NULL
+                else:
+                    assert field.remote_field.on_delete == models.CASCADE
+
+    def _get_model_remote_fields(self, model):
+        return [
+            field
+            for field in model._meta.get_fields()
+            if isinstance(field, models.ForeignKey)
+            or isinstance(field, models.OneToOneField)
+        ]
