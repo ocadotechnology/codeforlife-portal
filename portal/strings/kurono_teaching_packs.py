fef51a725ff599521b3cbe74893c0e347694f609
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-01-05 15:28:45 +0000

    Fix typos and grid spacing

--- portal/strings/kurono_teaching_packs.py
+++ portal/strings/kurono_teaching_packs.py
@@ -1,8 +1,8 @@
 # coding=utf-8
 KURONO_TEACHING_PACKS_BANNER = {
     "title": "Kurono Teaching Packs",
-    "subtitle": "We’ve created a comprehensive set of PDF teaching materials to help"
-                "you teach students the UK National Computing Curriculum throughout"
+    "subtitle": "We’ve created a comprehensive set of PDF teaching materials to help "
+                "you teach students the UK National Computing Curriculum throughout "
                 "their secondary school years.",
     "text": "",
     "image_class": "banner--picture--aimmo-teaching-packs",
