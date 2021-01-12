62bad407c8484e92b3854b6d6789008b4f2037ee
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-01-12 16:50:14 +0000

    Reduce resources subtext

--- portal/strings/teacher_resources.py
+++ portal/strings/teacher_resources.py
@@ -1,16 +1,16 @@
 # coding=utf-8
 RAPID_ROUTER_RESOURCES_BANNER = {
     "title": "Rapid Router Resources",
-    "subtitle": "We’ve created a comprehensive set of PDF teaching materials to help "
-    "you teach students Key Stages 1-3 of the UK National Computing Curriculum.",
+    "subtitle": "We’ve created a comprehensive set of teaching materials to help you "
+    "teach students the UK National Computing Curriculum.",
     "text": "",
     "image_class": "banner--picture--play",
 }
 
 KURONO_RESOURCES_BANNER = {
     "title": "Kurono Resources",
-    "subtitle": "We’ve created a comprehensive set of PDF teaching materials to help "
-    "you teach students Key Stages 3 and 4 of the UK National Computing Curriculum.",
+    "subtitle": "We’ve created a comprehensive set of teaching materials to help you "
+    "teach students the UK National Computing Curriculum.",
     "text": "",
     "image_class": "banner--picture--aimmo-resources",
 }
