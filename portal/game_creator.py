57dfe549a8e7266aaefcce6121aaaf118ae4e702
Razvan Mahu <razvan.mahu@ocado.com>
2021-01-25 11:51:20 +0000

    move avatar_creator to aimmo

--- portal/game_creator.py
+++ portal/game_creator.py
@@ -35,9 +35,7 @@
 # program; modified versions of the program must be marked as such and not
 # identified as the original program.
 
-import os
-
-from aimmo.models import Avatar, Game
+from aimmo.avatar_creator import create_avatar_for_user
 
 
 def create_game(main_user, form):
@@ -59,18 +57,3 @@ def create_game(main_user, form):
     game.save()
     create_avatar_for_user(main_user, game.id)
     return game
-
-
-def create_avatar_for_user(user, game_id):
-    """
-    Creates an Avatar object for a user. Sets the initial code to simple avatar code
-    (unless specified otherwise).
-
-    :param user: The user the Avatar is for.
-    :param game_id: The id of the game in which the Avatar is created.
-    :return: The initialised Avatar object.
-    """
-    game: Game = Game.objects.get(id=game_id)
-    initial_code = game.worksheet.starter_code
-    avatar = Avatar.objects.create(owner=user, code=initial_code, game_id=game_id)
-    return avatar
