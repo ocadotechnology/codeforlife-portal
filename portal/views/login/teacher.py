e4c27c4bcb9f5df90dffafc769e52177638dae4b
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-05-07 17:48:46 +0100

    Only do auth logic on auth step of login view

--- portal/views/login/teacher.py
+++ portal/views/login/teacher.py
@@ -41,25 +41,28 @@ class TeacherLoginView(LoginView):
         account, this redirects the user to the locked out page. However, if the lockout
         time is more than 24 hours before this is executed, the account is unlocked.
         """
-        form = self.get_form(data=self.request.POST)
+        wizard_goto_step = self.request.POST.get('wizard_goto_step', None)
 
-        email = request.POST.get("auth-username")
-        if Teacher.objects.filter(new_user__email=email).exists():
-            teacher = Teacher.objects.get(new_user__email=email)
+        if wizard_goto_step == 'auth':
+            form = self.get_form(data=self.request.POST)
 
-            if teacher.blocked_time is not None:
-                if has_user_lockout_expired(teacher):
-                    teacher.blocked_time = None
-                    teacher.save()
-                else:
-                    return render(
-                        self.request,
-                        "portal/locked_out.html",
-                        {"is_teacher": True},
-                    )
+            email = request.POST.get("auth-username")
+            if Teacher.objects.filter(new_user__email=email).exists():
+                teacher = Teacher.objects.get(new_user__email=email)
 
-        if form.is_valid():
-            # Reset ratelimit cache upon successful login
-            clear_ratelimit_cache()
+                if teacher.blocked_time is not None:
+                    if has_user_lockout_expired(teacher):
+                        teacher.blocked_time = None
+                        teacher.save()
+                    else:
+                        return render(
+                            self.request,
+                            "portal/locked_out.html",
+                            {"is_teacher": True},
+                        )
+
+            if form.is_valid():
+                # Reset ratelimit cache upon successful login
+                clear_ratelimit_cache()
 
         return super(TeacherLoginView, self).post(request, *args, **kwargs)
