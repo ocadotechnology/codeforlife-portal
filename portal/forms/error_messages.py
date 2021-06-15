8377329e5fae5a22aaaeef9f1d2e8ae0d2781fc3
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-06-15 14:08:43 +0100

    Use single line string

--- portal/forms/error_messages.py
+++ portal/forms/error_messages.py
@@ -1,2 +1,1 @@
-INVALID_LOGIN_MESSAGE = """Something is wrong! Please check that you typed your details 
-correctly and that you have verified your account via email."""
+INVALID_LOGIN_MESSAGE = "Something is wrong! Please check that you typed your details correctly and that you have verified your account via email."
