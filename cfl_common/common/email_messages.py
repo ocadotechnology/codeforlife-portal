830d4464a350eea1d7f3b112c94ad0df51b17d15
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-06-16 17:46:17 +0100

    fix: Check for existing user on email change + change username

--- cfl_common/common/email_messages.py
+++ cfl_common/common/email_messages.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Code for Life
 #
-# Copyright (C) 2019, Ocado Innovation Limited
+# Copyright (C) 2021, Ocado Innovation Limited
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -38,53 +38,60 @@ from django.urls import reverse
 
 
 def emailSubjectPrefix():
-    return "Code for Life"
+    return f"Code for Life"
 
 
 def emailBodySignOff(request):
-    return "\n\nThanks,\n\nThe Code for Life team.\n" + request.build_absolute_uri(
-        reverse("home")
+    return (
+        f"\n\nThanks,\n\nThe Code for Life team.\n"
+        f"{request.build_absolute_uri(reverse('home'))}"
     )
 
 
 def emailVerificationNeededEmail(request, token):
     return {
-        "subject": emailSubjectPrefix() + " : Email address verification needed",
+        "subject": f"{emailSubjectPrefix()}: Email address verification needed",
         "message": (
-            "Please go to "
-            + request.build_absolute_uri(
-                reverse("verify_email", kwargs={"token": token})
-            )
-            + " to verify your email address."
-            + emailBodySignOff(request)
+            f"Please go to "
+            f"{request.build_absolute_uri(reverse('verify_email', kwargs={'token': token}))} "
+            f"to verify your email address."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def emailChangeVerificationEmail(request, token):
     return {
-        "subject": emailSubjectPrefix() + " : Email address verification needed",
+        "subject": f"{emailSubjectPrefix()}: Email address verification needed",
         "message": (
-            "You are changing your email, please go to "
-            + request.build_absolute_uri(
-                reverse("verify_email", kwargs={"token": token})
-            )
-            + " to verify your new email address. If you are not part of Code for Life "
-            + "then please ignore this email. "
-            + emailBodySignOff(request)
+            f"You are changing your email, please go to "
+            f"{request.build_absolute_uri(reverse('verify_email', kwargs={'token': token}))} "
+            f"to verify your new email address. If you are not part of Code for Life "
+            f"then please ignore this email."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def emailChangeNotificationEmail(request):
     return {
-        "subject": emailSubjectPrefix() + " : Email address changed",
+        "subject": f"{emailSubjectPrefix()}: Email address changed",
         "message": (
-            "Someone has tried to change the email address of your account. If this was "
-            + "not you, please get in contact with us via "
-            + request.build_absolute_uri(reverse("help"))
-            + "#contact ."
-            + emailBodySignOff(request)
+            f"Someone has tried to change the email address of your account. If this "
+            f"was not you, please get in contact with us via "
+            f"{request.build_absolute_uri(reverse('help'))}#contact."
+            f"{emailBodySignOff(request)}"
+        ),
+    }
+
+
+def emailChangeDuplicateNotificationEmail(request, email):
+    return {
+        "subject": f"{emailSubjectPrefix()}: Duplicate account error",
+        "message": (
+            f"A user is already registered with this email address: {email}.\n"
+            f"Please change your email address to something else."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
@@ -96,15 +103,13 @@ def userAlreadyRegisteredEmail(request, email, is_independent_student=False):
         login_url = reverse("teacher_login")
 
     return {
-        "subject": emailSubjectPrefix() + ": Duplicate account error",
+        "subject": f"{emailSubjectPrefix()}: Duplicate account error",
         "message": (
-            "A user is already registered with this email address: "
-            + email
-            + ".\nIf you've already registered, please login: "
-            + request.build_absolute_uri(login_url)
-            + ".\n"
-            + "Otherwise please register with a different email address."
-            + emailBodySignOff(request)
+            f"A user is already registered with this email address: {email}.\n"
+            f"If you've already registered, please login: "
+            f"{request.build_absolute_uri(login_url)}.\n"
+            f"Otherwise please register with a different email address."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
@@ -112,165 +117,140 @@ def userAlreadyRegisteredEmail(request, email, is_independent_student=False):
 def indepStudentUsernameAlreadyExistsEmail(request, username):
     return {
         "subject": f"{emailSubjectPrefix()}: Username already taken",
-        "message": f"A user is already registered with this username: {username}.\n"
-        f"If you've already registered, please login: {request.build_absolute_uri(reverse('independent_student_login'))}.\n"
-        f"Otherwise please register with a different username."
-        f"{emailBodySignOff(request)}",
+        "message":
+            f"A user is already registered with this username: {username}.\n"
+            f"If you've already registered, please login: "
+            f"{request.build_absolute_uri(reverse('independent_student_login'))}.\n"
+            f"Otherwise please register with a different username."
+            f"{emailBodySignOff(request)}",
     }
 
 
 def joinRequestPendingEmail(request, pendingAddress):
     return {
-        "subject": emailSubjectPrefix() + " : School or club join request pending",
+        "subject": f"{emailSubjectPrefix()}: School or club join request pending",
         "message": (
-            "Someone with the email address '"
-            + pendingAddress
-            + "' has asked to join your school or club, please go to "
-            + request.build_absolute_uri(reverse("dashboard"))
-            + " to view the pending join request."
-            + emailBodySignOff(request)
+            f"Someone with the email address '{pendingAddress}' has asked to join your "
+            f"school or club, please go to "
+            f"{request.build_absolute_uri(reverse('dashboard'))} to view the pending "
+            f"join request."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def joinRequestSentEmail(request, schoolName):
     return {
-        "subject": emailSubjectPrefix() + " : School or club join request sent",
+        "subject": f"{emailSubjectPrefix()}: School or club join request sent",
         "message": (
-            "Your request to join the school or club '"
-            + schoolName
-            + "' has been sent. Someone will either accept or deny your request soon."
-            + emailBodySignOff(request)
+            f"Your request to join the school or club '{schoolName}' has been sent. "
+            f"Someone will either accept or deny your request soon."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def joinRequestAcceptedEmail(request, schoolName):
     return {
-        "subject": emailSubjectPrefix() + " : School or club join request accepted",
+        "subject": f"{emailSubjectPrefix()}: School or club join request accepted",
         "message": (
-            "Your request to join the school or club '"
-            + schoolName
-            + "' has been accepted."
-            + emailBodySignOff(request)
+            f"Your request to join the school or club '{schoolName}' has been accepted."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def joinRequestDeniedEmail(request, schoolName):
     return {
-        "subject": emailSubjectPrefix() + " : School or club join request denied",
+        "subject": f"{emailSubjectPrefix()}: School or club join request denied",
         "message": (
-            "Your request to join the school or club '"
-            + schoolName
-            + "' has been denied. If you think this was in error you should speak to the "
-            + "administrator of that school or club."
-            + emailBodySignOff(request)
+            f"Your request to join the school or club '{schoolName}' has been denied. "
+            f"If you think this was in error you should speak to the administrator of "
+            f"that school or club."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def kickedEmail(request, schoolName):
     return {
-        "subject": emailSubjectPrefix()
-        + " : You were removed from your school or club",
+        "subject": f"{emailSubjectPrefix()}: You were removed from your school or club",
         "message": (
-            "You have been removed from the school or club '"
-            + schoolName
-            + "'. If you think this was in error, please contact the administrator of that school or club."
-            + emailBodySignOff(request)
+            f"You have been removed from the school or club '{schoolName}'. "
+            f"If you think this was an error, please contact the administrator of that "
+            f"school or club."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def adminGivenEmail(request, schoolName):
     return {
-        "subject": emailSubjectPrefix()
-        + " : You have been made a school or club administrator",
+        "subject": f"{emailSubjectPrefix()}: You have been made a school or club administrator",
         "message": (
-            "Administrator control of the school or club '"
-            + schoolName
-            + "' has been given to you. Go to "
-            + request.build_absolute_uri(reverse("dashboard"))
-            + " to start managing your school or club."
-            + emailBodySignOff(request)
+            f"Administrator control of the school or club '{schoolName}' has been "
+            f"given to you. Go to {request.build_absolute_uri(reverse('dashboard'))} "
+            f"to start managing your school or club."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def adminRevokedEmail(request, schoolName):
     return {
-        "subject": emailSubjectPrefix()
-        + " : You are no longer a school or club administrator",
+        "subject": f"{emailSubjectPrefix()}: You are no longer a school or club administrator",
         "message": (
-            "Your administrator control of the school or club '"
-            + schoolName
-            + "' has been revoked. If you think this is in error, please contact one of "
-            + "the other administrators in your school or club."
-            + emailBodySignOff(request)
+            f"Your administrator control of the school or club '{schoolName}' has been "
+            f"revoked. If you think this is an error, please contact one of the other "
+            f"administrators in your school or club."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def studentJoinRequestSentEmail(request, schoolName, accessCode):
     return {
-        "subject": emailSubjectPrefix() + " : School or club join request sent",
+        "subject": f"{emailSubjectPrefix()}: School or club join request sent",
         "message": (
-            "Your request to join the school or club '"
-            + schoolName
-            + "' in class "
-            + accessCode
-            + " has been sent to that class's teacher, who will either accept "
-            + "or deny your request."
-            + emailBodySignOff(request)
+            f"Your request to join the school or club '{schoolName}' in class "
+            f"{accessCode} has been sent to that class's teacher, who will either "
+            f"accept or deny your request."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def studentJoinRequestNotifyEmail(request, username, email, accessCode):
     return {
-        "subject": emailSubjectPrefix()
-        + " : School or club join request by student "
-        + username,
+        "subject": f"{emailSubjectPrefix()}: School or club join request by student {username}",
         "message": (
-            "There is a request waiting from student with username '"
-            + username
-            + "' and email "
-            + email
-            + " to join your class "
-            + accessCode
-            + ". Go to "
-            + request.build_absolute_uri(reverse("dashboard"))
-            + " to review the request."
-            + emailBodySignOff(request)
+            f"There is a request waiting from student with username '{username}' and "
+            f"email {email} to join your class {accessCode}. Go to "
+            f"{request.build_absolute_uri(reverse('dashboard'))} to review the request."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def studentJoinRequestRejectedEmail(request, schoolName, accessCode):
     return {
-        "subject": emailSubjectPrefix() + " : School or club join request rejected",
+        "subject": f"{emailSubjectPrefix()}: School or club join request rejected",
         "message": (
-            "Your request to join the school or club '"
-            + schoolName
-            + "' in class "
-            + accessCode
-            + " has been rejected. Speak to your teacher if you think this is "
-            + "in error."
-            + emailBodySignOff(request)
+            f"Your request to join the school or club '{schoolName}' in class "
+            f"{accessCode} has been rejected. Speak to your teacher if you think this "
+            f"is an error."
+            f"{emailBodySignOff(request)}"
         ),
     }
 
 
 def inviteTeacherEmail(request):
     return {
-        "subject": emailSubjectPrefix()
-        + " : You've been invited to join Code for Life",
+        "subject": f"{emailSubjectPrefix()}: You've been invited to join Code for Life",
         "message": (
-            "A colleague at your school or code club has invited you to become part of Code "
-            + "for Life.\n\nPlease register your details to get started.\n\n"
-            + request.build_absolute_uri(reverse("register"))
-            + "\n"
-            + emailBodySignOff(request)
+            f"A colleague at your school or code club has invited you to become part of "
+            f"Code for Life.\n\nPlease register your details to get started.\n\n"
+            f"{request.build_absolute_uri(reverse('register'))}"
+            f"{emailBodySignOff(request)}"
         ),
     }
