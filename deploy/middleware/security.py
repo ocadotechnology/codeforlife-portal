8d5c0b23686b7aa74c24dbe797a4442428710094
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-06-22 17:07:14 +0100

    Extend middleware directly instead

--- deploy/middleware/security.py
+++ deploy/middleware/security.py
@@ -1,63 +1,22 @@
-import re
-
-from django.conf import settings
-from django.http import HttpResponsePermanentRedirect
-from django.utils.deprecation import MiddlewareMixin
+from django.middleware.security import SecurityMiddleware
 
 
-class SecurityMiddleware(MiddlewareMixin):
+class CustomSecurityMiddleware(SecurityMiddleware):
     """
-    Follows the architecture of Django's Security Middleware.
+    Extends Django's Security Middleware.
     See https://docs.djangoproject.com/en/2.2/_modules/django/middleware/security/ for
     the source code, as well as https://docs.djangoproject.com/en/2.2/ref/middleware/#module-django.middleware.security
     for docs on security middleware.
     """
 
-    def __init__(self, get_response=None):
-        self.sts_seconds = settings.SECURE_HSTS_SECONDS
-        self.sts_include_subdomains = settings.SECURE_HSTS_INCLUDE_SUBDOMAINS
-        self.sts_preload = settings.SECURE_HSTS_PRELOAD
-        self.content_type_nosniff = settings.SECURE_CONTENT_TYPE_NOSNIFF
-        self.xss_filter = settings.SECURE_BROWSER_XSS_FILTER
-        self.redirect = settings.SECURE_SSL_REDIRECT
-        self.redirect_host = settings.SECURE_SSL_HOST
-        self.redirect_exempt = [re.compile(r) for r in settings.SECURE_REDIRECT_EXEMPT]
-        self.get_response = get_response
-
-    def process_request(self, request):
-        path = request.path.lstrip("/")
-        if (
-            self.redirect
-            and not request.is_secure()
-            and not any(pattern.search(path) for pattern in self.redirect_exempt)
-        ):
-            host = self.redirect_host or request.get_host()
-            return HttpResponsePermanentRedirect(
-                "https://%s%s" % (host, request.get_full_path())
-            )
-
     def process_response(self, request, response):
         """
-        Diverges from the original security middleware to ensure the
-        X-XSS-Protection header is set to 0.
+        Extends the original security middleware to ensure the X-XSS-Protection header
+        is set to 0.
         """
-        if (
-            self.sts_seconds
-            and request.is_secure()
-            and "Strict-Transport-Security" not in response
-        ):
-            sts_header = "max-age=%s" % self.sts_seconds
-            if self.sts_include_subdomains:
-                sts_header = sts_header + "; includeSubDomains"
-            if self.sts_preload:
-                sts_header = sts_header + "; preload"
-            response["Strict-Transport-Security"] = sts_header
+        super().process_response(request, response)
 
-        if self.content_type_nosniff:
-            response.setdefault("X-Content-Type-Options", "nosniff")
-
-        # Custom behaviour
         if self.xss_filter:
-            response.setdefault("X-XSS-Protection", "0")
+            response["X-XSS-Protection"] = "0"
 
         return response
--- portal/autoconfig.py
+++ portal/autoconfig.py
@@ -125,7 +125,7 @@ SETTINGS = {
         "django.middleware.locale.LocaleMiddleware",
         "django.middleware.common.CommonMiddleware",
         "django.middleware.csrf.CsrfViewMiddleware",
-        "deploy.middleware.security.SecurityMiddleware",
+        "deploy.middleware.security.CustomSecurityMiddleware",
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
         "django.middleware.clickjacking.XFrameOptionsMiddleware",
