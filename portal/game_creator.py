053761b04b50158b91ba1ff7c59973cc82bd05f9
Razvan Mahu <razvan.mahu@ocado.com>
2021-07-09 16:43:20 +0100

    create auth token and k8s secret on game creation

--- portal/game_creator.py
+++ portal/game_creator.py
@@ -35,7 +35,18 @@
 # program; modified versions of the program must be marked as such and not
 # identified as the original program.
 
+import secrets
 from aimmo.avatar_creator import create_avatar_for_user
+from aimmo.game_manager import GameManager
+
+NUM_BYTES_FOR_TOKEN_GENERATOR = 32
+TOKEN_MAX_LENGTH = 48
+
+
+def generate_game_token():
+    return secrets.token_urlsafe(nbytes=NUM_BYTES_FOR_TOKEN_GENERATOR)[
+        :TOKEN_MAX_LENGTH
+    ]
 
 
 def create_game(main_user, form):
@@ -51,9 +62,12 @@ def create_game(main_user, form):
     :return: The initialised Game object.
     """
     game = form.save(commit=False)
+    game.auth_token = generate_game_token()
     game.generator = "Main"
     game.owner = main_user
     game.main_user = main_user
     game.save()
     create_avatar_for_user(main_user, game.id)
+    game_manager = GameManager()
+    game_manager.create_game_secret(game_id=game.id, token=game.auth_token)
     return game
