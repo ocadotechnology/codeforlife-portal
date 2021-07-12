aabc7aa2f14aa7f510f02f0f5709a2f4d931590c
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-07-12 22:22:59 +0100

    fix: Remove score setting and use env var instead

--- test_settings.py
+++ test_settings.py
@@ -74,6 +74,3 @@ COOKIE_MANAGEMENT_ENABLED = False
 from django_autoconfig.autoconfig import configure_settings
 
 configure_settings(globals())
-
-# Needs to be after the autoconfig import to override the main score setting
-RECAPTCHA_REQUIRED_SCORE = 0
