aa6795aa0d90d9391a3e5485a3b126f609c83caa
Dioni Zhong <dioni.zhong@ocado.com>
2021-07-22 11:10:08 +0100

    feat: add contributor pages

--- portal/strings/about.py
+++ portal/strings/about.py
@@ -5,3 +5,15 @@ ABOUT_BANNER = {
     "text": "",
     "image_class": "banner--picture--about",
 }
+
+GETINVOLVED_BANNER = {
+    "title": "Get involved",
+    "subtitle": "How you can get involved with Code for Life products and resources",
+    "image_class": "banner--picture--getinvolved",
+}
+
+CONTRIBUTE_BANNER = {
+    "title": "Contributing to Code for Life",
+    "subtitle": "How you can get involved with Code for Life products and resources and help us help others",
+    "image_class": "banner--picture--contribute",
+}
