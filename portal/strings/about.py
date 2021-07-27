eead3b752f78b171fe3d593ca2c213e70b41f6f2
Dioni Zhong <dioni.zhong@ocado.com>
2021-07-27 17:36:15 +0100

    fix: reformatting, text updates

--- portal/strings/about.py
+++ portal/strings/about.py
@@ -8,12 +8,12 @@ ABOUT_BANNER = {
 
 GETINVOLVED_BANNER = {
     "title": "Get involved",
-    "subtitle": "How you can get involved with Code for Life products and resources",
+    "subtitle": "How you can get involved with the creation of Code for Life products and resources",
     "image_class": "banner--picture--getinvolved",
 }
 
 CONTRIBUTE_BANNER = {
-    "title": "Contributing to Code for Life",
+    "title": "Contribute to Code for Life",
     "subtitle": "How you can get involved with Code for Life products and resources and help us help others",
     "image_class": "banner--picture--contribute",
 }
