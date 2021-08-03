47c683346c17eba91e7e574c95ef04836e499414
Dioni Zhong <dioni.zhong@ocado.com>
2021-08-03 14:25:38 +0100

    feat: add contributor pages (#1493)
    * feat: add contributor pages
    
    * fix: some renaming, formatting
    
    * fix: resize img, formatting, test
    
    * Merge branch 'master' into contributor
    
    * fix: reformatting, text updates
    
    * fix: use fixed height for card images
    
    * fix: tinify images
    
    * fix: img updates, reformatting, display fixes
    
    * fix: reformat text and images
    
    * fix: fix card image
    
    * fix: test updates, links to new page

--- portal/views/about.py
+++ portal/views/about.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 
-from portal.strings.about import ABOUT_BANNER
+from portal.strings.about import ABOUT_BANNER, GETINVOLVED_BANNER, CONTRIBUTE_BANNER
 
 
 def about(request):
     return render(request, "portal/about.html", {"BANNER": ABOUT_BANNER})
+
+
+def getinvolved(request):
+    return render(request, "portal/getinvolved.html", {"BANNER": GETINVOLVED_BANNER})
+
+
+def contribute(request):
+    return render(request, "portal/contribute.html", {"BANNER": CONTRIBUTE_BANNER})
