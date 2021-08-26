656dff706fda0490071b79eb29a7f1351ed90f63
Dioni Zhong <dioni.zhong@ocado.com>
2021-08-26 18:20:44 +0100

    fix: buttons and other adjustments

--- portal/strings/teacher_resources.py
+++ portal/strings/teacher_resources.py
@@ -2,7 +2,7 @@
 RAPID_ROUTER_RESOURCES_BANNER = {
     "title": "Rapid Router Resources",
     "subtitle": "We’ve created a comprehensive set of teaching materials to help you "
-    "teach students the UK National Computing Curriculum.",
+    "teach students Computing.",
     "text": "",
     "image_class": "banner--picture--play",
 }
@@ -10,7 +10,7 @@ RAPID_ROUTER_RESOURCES_BANNER = {
 KURONO_RESOURCES_BANNER = {
     "title": "Kurono Resources",
     "subtitle": "We’ve created a comprehensive set of teaching materials to help you "
-    "teach students the UK National Computing Curriculum.",
+    "teach students Computing.",
     "text": "",
     "image_class": "banner--picture--aimmo-resources",
 }
