a00b6b115337ee028e058dc936fda956b20fe2fa
Dioni Zhong <dioni.zhong@ocado.com>
2021-09-02 17:15:52 +0100

    fix: rebranding adjustments (#1521)
    * fix: rebranding adjustments
    
    * update snapshot to fix test
    
    * fix: clean up and mobile amend

--- portal/forms/teach.py
+++ portal/forms/teach.py
@@ -57,18 +57,16 @@ class TeacherSignupForm(forms.Form):
     teacher_first_name = forms.CharField(
         label="First name",
         max_length=100,
-        widget=forms.TextInput(attrs={"autocomplete": "off", "placeholder": "Grace"}),
+        widget=forms.TextInput(attrs={"autocomplete": "off"}),
     )
     teacher_last_name = forms.CharField(
         label="Last name",
         max_length=100,
-        widget=forms.TextInput(attrs={"autocomplete": "off", "placeholder": "Hopper"}),
+        widget=forms.TextInput(attrs={"autocomplete": "off"}),
     )
     teacher_email = forms.EmailField(
         label="Email address",
-        widget=forms.EmailInput(
-            attrs={"autocomplete": "off", "placeholder": "grace.hopper@navy.mil"}
-        ),
+        widget=forms.EmailInput(attrs={"autocomplete": "off"}),
     )
 
     newsletter_ticked = forms.BooleanField(
