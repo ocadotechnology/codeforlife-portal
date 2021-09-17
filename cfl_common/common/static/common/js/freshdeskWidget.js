29fbe7c3f86ca8a56889c68694e95060fd45dd3e
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-09-17 11:34:49 +0100

    Allow more directories (add missing slashes)

---
+++ cfl_common/common/static/common/js/freshdeskWidget.js
@@ -0,0 +1,4 @@
+window.fwSettings={
+    'widget_id':77000000397
+};
+!function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
