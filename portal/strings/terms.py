afe9d898dbbd1aab5970911870ad5dd68f0812df
Florian Aucomte <f.aucomte@hotmail.co.uk>
2021-11-19 11:11:53 +0000

    feat: Update terms & private policy pages designs

--- portal/strings/terms.py
+++
@@ -1,6 +0,0 @@
-TERMS_BANNER = {
-    "title": "Terms of use",
-    "subtitle": "",
-    "text": "",
-    "image_class": "banner--picture--terms",
-}
--- portal/views/privacy_policy.py
+++ portal/views/privacy_policy.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 
-from portal.strings.privacy_policy import PRIVACY_POLICY_BANNER
-
 
 def privacy_policy(request):
     return render(
         request,
         "portal/privacy_policy.html",
-        {"BANNER": PRIVACY_POLICY_BANNER, "last_updated": "3rd September 2021"},
+        {"last_updated": "3rd September 2021"},
     )
