cd2be5e98964629635e7b0ceb654127d0b8aa188
Dioni Zhong <dioni.zhong@ocado.com>
2021-11-19 15:48:32 +0000

    text updates

--- portal/strings/home_learning.py
+++ portal/strings/home_learning.py
@@ -1,6 +1,6 @@
 HOME_LEARNING_BANNER = {
     "title": "Home learning",
-    "subtitle": "Whether you're a parent, a caregiver, or a motivated student — our Rapid Router game is easy to use "
+    "subtitle": "Whether you're a parent, a caregiver, or a curious student — our Rapid Router game is easy to use "
     "and free – forever.",
     "text": "",
     "image_class": "banner--picture--homelearning",
