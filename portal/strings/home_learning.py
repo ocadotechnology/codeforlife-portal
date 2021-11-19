9e02cef6e6d8fe11e17645e2747cf1554a4fddac
Dioni Zhong <dioni.zhong@ocado.com>
2021-11-19 10:55:25 +0000

    feat: home learning updates

--- portal/strings/home_learning.py
+++ portal/strings/home_learning.py
@@ -1,8 +1,7 @@
 HOME_LEARNING_BANNER = {
-    "title": "#KeepKidsCoding",
-    "subtitle": "Teach your child to code at home — our Rapid Router game supports the "
-    "National Curriculum Computing strand, is easy to teach and free to "
-    "use – forever.",
+    "title": "Home learning",
+    "subtitle": "Teach your child to code at home — our Rapid Router game is easy to teach "
+    "and free to use – forever.",
     "text": "",
     "image_class": "banner--picture--educate",
 }
