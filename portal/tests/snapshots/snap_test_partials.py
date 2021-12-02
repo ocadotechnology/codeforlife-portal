c7c5782f0bad4ae68addaa88786c962ba8fec69e
Razvan Mahu <67904187+razvan-pro@users.noreply.github.com>
2021-12-02 15:37:39 +0000

    fix: align header and footer (#1654)
    * fix: align header and footer
    
    * update snapshot
    
    * Merge branch 'master' into align-header-and-footer
    
    * Merge master into align-header-and-footer
    
    * Merge master into align-header-and-footer

--- portal/tests/snapshots/snap_test_partials.py
+++ portal/tests/snapshots/snap_test_partials.py
@@ -7,21 +7,27 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_banner 1'] = '''<div class="banner banner--teacher row mx-0">
-    <div class="col-sm-5 col-sm-offset-2">
-        <h1 class="banner__text--primary">Test title</h1>
-        
-            <h4>Test subtitle</h4>
-        
-        
-            <p>Test text</p>
-        
-    </div>
-    <div class="col-sm-5">
-        <div class="banner--picture">
-            <div class="banner--picture__inside1">
-                <div class="banner--picture__inside2 test--image--class"
-                     >
+snapshots['test_banner 1'] = '''<div class="banner banner--teacher">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 d-flex">
+                <div>
+                    <h1 class="banner__text--primary">Test title</h1>
+                    
+                        <h4>Test subtitle</h4>
+                    
+                    
+                        <p>Test text</p>
+                    
+                </div>
+                <div>
+                    <div class="banner--picture">
+                        <div class="banner--picture__inside1">
+                            <div class="banner--picture__inside2 test--image--class"
+                                 >
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
