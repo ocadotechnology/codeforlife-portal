d028f097d593e70e7010f4a06ce9799b0fd0e339
Razvan Mahu <67904187+razvan-pro@users.noreply.github.com>
2021-12-16 12:43:20 +0200

    fix: remove wagtail (#1677)
    * fix: remove wagtail
    
    * Merge master into remove-wagtail

--- portal/urls.py
+++ portal/urls.py
@@ -16,9 +16,6 @@ from two_factor.views import (
     SetupCompleteView,
     SetupView,
 )
-from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.core import urls as wagtail_urls
-from wagtail.documents import urls as wagtaildocs_urls
 
 from portal.helpers.decorators import ratelimit
 from portal.helpers.ratelimit import (
@@ -455,9 +452,6 @@ urlpatterns = [
             ]
         ),
     ),
-    url(r"^cms/", include(wagtailadmin_urls)),
-    url(r"^documents/", include(wagtaildocs_urls)),
-    url(r"^pages/", include(wagtail_urls)),
 ]
 
 
