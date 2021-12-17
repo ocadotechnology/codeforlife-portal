571850fe2e8aea1996a69dea1e55747f1807a3db
Razvan Mahu <razvan.mahu@ocado.com>
2021-12-17 16:24:19 +0000

    Revert "decrease FADE_TIME"
    This reverts commit ecab1948150dd99b448271474def5e07a9d20c16.

--- portal/tests/pageObjects/portal/base_page.py
+++ portal/tests/pageObjects/portal/base_page.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 
-FADE_TIME = 0.2
+FADE_TIME = 0.3
 
 
 class BasePage(object):
