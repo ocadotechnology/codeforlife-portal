722a7c373f61ee311bd1d9b87028b5344dac3097
KamilPawel <kamil.sosinski@ocado.com>
2022-02-01 11:12:41 +0000

    fix: moved styles around, and cleaned up whitespaces

--- portal/two_factor/form.py
+++ portal/two_factor/form.py
@@ -15,4 +15,4 @@ except ImportError:
 # wants to disable 2FA after clicking disable
 # 2FA, setting it to always checked and hidden in CSS
 class DisableForm(forms.Form):
-    understand = forms.BooleanField(label="Hello", initial=True)
+    understand = forms.BooleanField(label="", initial=True)
