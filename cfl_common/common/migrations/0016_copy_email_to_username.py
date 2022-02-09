d2f36df93215a6299f33855d9aa93e150ae74525
KamilPawel <kamil.sosinski@ocado.com>
2022-02-09 12:28:25 +0000

    fix: cleaned up the is_active statement

--- cfl_common/common/migrations/0016_copy_email_to_username.py
+++ cfl_common/common/migrations/0016_copy_email_to_username.py
@@ -3,8 +3,8 @@ from django.db import migrations
 
 def copy_email_to_username(apps, schema):
     Student = apps.get_model("common", "Student")
-    independent_students = Student.objects.filter(class_field=None)
-    for student in independent_students:
+    Student.objects.filter(class_field__isnull=True, new_user__is_active=True)
+    for student in Students:
         if student.new_user.is_active:
             student.new_user.username = student.new_user.email
             student.new_user.save()
