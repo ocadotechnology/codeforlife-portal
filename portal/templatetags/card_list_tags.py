8a7f5d9ce8213617bf1949bb0df8670e54be9125
Razvan Mahu <razvan.mahu@ocado.com>
2022-02-23 16:54:26 +0000

    wip: student aimmo dashboard

--- portal/templatetags/card_list_tags.py
+++ portal/templatetags/card_list_tags.py
@@ -11,9 +11,5 @@ def card_list(context):
     - image: the path to the card's image (top-half)
     - title: the heading of the card
     - description (optional): the text paragraph of the card
-    - thumbnail_text (optional): text to be shown as the thumbnail
-    - thumbnail_image (optional): the path to the image to be shown as the thumbnail
-    - button_text (optional): the text on the card's button
-    - button_link (optional): the link that the button redirects to
     """
     return context["CARD_LIST"]
