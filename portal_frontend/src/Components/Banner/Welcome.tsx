e79813dc0a4c5af4788b3cf2f295603b11d32636
Dioni Zhong <dioni.zhong@ocado.com>
2022-02-23 14:50:01 +0000

    feat: add react welcome banner and tertiary color

---
+++ portal_frontend/src/Components/Banner/Welcome.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Box from '@mui/material/Box';
+import Typography from "@mui/material/Typography";
+
+interface Username {
+  name: string;
+  userType: string;
+}
+const Welcome: React.FC<Username> = ({ name, userType }) => {
+  let bgcolor, color;
+  if (userType === "independent") {
+    bgcolor = "secondary.main"
+    color = "secondary.contrastText"
+  } else if (userType === "teacher") {
+    bgcolor = "primary.main"
+    color = "primary.contrastText"
+  } else if (userType === "student") {
+    bgcolor = "tertiary.main"
+    color = "tertiary.contrastText"
+  }
+
+  const subtitle = (userType === "independent") ? (
+    <Typography variant="h4">
+      This is where you can access your games</Typography>)
+    : ""
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        height: "320px",
+        justifyContent: "center",
+        bgcolor: bgcolor,
+        color: color,
+        alignItems: "center"
+      }}
+    >
+      <Typography variant="h1">Welcome, {name}</Typography>
+      {subtitle}
+    </Box>
+  );
+};
+
+export default Welcome;
