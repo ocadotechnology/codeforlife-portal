34b9d191ef98006b5d8493dae22ec8d6e41ebbac
Dioni Zhong <dioni.zhong@ocado.com>
2022-02-28 15:03:49 +0000

    remove unused import

--- portal_frontend/src/Components/Banner/RapidRouter.tsx
+++ portal_frontend/src/Components/Banner/RapidRouter.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Box from '@mui/material/Box';
-import Typography from "@mui/material/Typography";
 
 import RRBackground from "../../img/rapid_router_landing_hero.png";
 import RRLogo from "../../img/RR_logo.svg";
