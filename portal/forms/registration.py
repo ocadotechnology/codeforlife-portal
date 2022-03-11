872fcd384d0cf7cbd51ea7b228a2b0d96451fa7c
KamilPawel <kamil.sosinski@ocado.com>
2022-03-11 14:51:24 +0000

    reading the docs

--- portal/forms/registration.py
+++ portal/forms/registration.py
@@ -77,10 +77,10 @@ class TeacherPasswordResetForm(forms.Form):
         # Email subject *must not* contain newlines
         subject = "".join(subject.splitlines())
         body = loader.render_to_string(email_template_name, context)
-        html_email = loader.render_to_string(html_email_template_name, context)
 
         email_message = EmailMultiAlternatives(subject, body, from_email, [to_email])
         if html_email_template_name is not None:
+            html_email = loader.render_to_string(html_email_template_name, context)
             email_message.attach_alternative(html_email, "text/html")
 
         email_message.send()
