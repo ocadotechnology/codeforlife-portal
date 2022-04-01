dd511af1f8dfb03f3079f0b4703a173aa5ea53f4
KamilPawel <kamil.sosinski@ocado.com>
2022-04-01 11:54:17 +0100

    email img

--- portal/forms/registration.py
+++ portal/forms/registration.py
@@ -116,7 +116,7 @@ class PasswordResetForm(forms.Form):
                     email_subject_content["subject"],
                     email_subject_content["message"],
                     email_subject_content["subject"],
-                    image_path=f"{domain}",
+                    image_path=f"{domain[:domain.index('http') + 4] + 's' + domain[domain.index('http') + 4]}",
                 )
 
     def _compute_protocol(self, use_https):
