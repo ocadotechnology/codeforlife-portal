732367c0a13b462ee50d0a6255097ee0023d0332
KamilPawel <kamil.sosinski@ocado.com>
2022-04-28 14:52:05 +0100

    add stuff

--- cfl_common/setup.py
+++ cfl_common/setup.py
@@ -4,7 +4,7 @@ import re
 import os
 from setuptools import find_packages, setup
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 with open(os.path.join(BASE_DIR, "portal/__init__.py"), "r") as fd:
     version = re.search(
