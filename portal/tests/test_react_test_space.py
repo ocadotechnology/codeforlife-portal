5fd687b6be1d84e7efbfab80d44e6e2bcc2b4f31
KamilPawel <kamil.sosinski@ocado.com>
2022-05-10 09:36:40 +0100

    add tests to reactTestSpace

--- portal/tests/test_react_test_space.py
+++ portal/tests/test_react_test_space.py
@@ -1,30 +1,26 @@
-import pytest
-from aimmo.models import Game
-from aimmo.worksheets import WORKSHEETS
-from common.models import Class
 from django.test.client import Client
 from django.urls.base import reverse
 
+from .base_test import BaseTest
 
-@pytest.mark.django_db
-def test_react_test_space_denied_access(teacher1):
-    """
-    Given you are logged in as a superuser
-    you can access react test space. Otherwise, you cannot access it.
-    """
+from django.contrib.auth.models import User
 
-    c = Client()
-    url = reverse("teacher_login")
-    print(url)
-    data = {
-        "username": "codeforlife-portal@ocado.com",
-        "password": "abc123",
-    }
-    c.get("/login/teacher/")
-    c.post("/login/teacher/", data)
 
-    teacher_dashboard_url = reverse("reactTestSpace")
+class TestReactTestSpace(BaseTest):
+    def test_access_denined(self):
+        c = Client()
+        response = c.get("/reactTestSpace/")
+        assert response.status_code == 403
 
-    response = c.get(teacher_dashboard_url)
+    def test_restrictions(self):
+        password = "abc123"
 
-    assert response.status_code == 301
+        admin = User.objects.create_superuser("admin", "admin@admin.com", password)
+
+        c = Client()
+
+        c.login(username=admin.username, password=password)
+
+        response = c.get("/reactTestSpace/")
+
+        assert response.status_code == 200
