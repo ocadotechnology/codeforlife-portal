5eda44bc5f2f6735b34c06a0d8c62a73e4a07719
faucomte97 <florian.aucomt1@ocado.com>
2022-06-10 04:29:03 +0100

    revert (wrong branch)

--- portal/tests/test_newsletter_footer.py
+++ portal/tests/test_newsletter_footer.py
@@ -6,7 +6,7 @@ class TestNewsletterFooter(TestCase):
     def test_newsletter_signup_successful(self):
         url = reverse("process_newsletter_form")
         client = Client()
-        data = {"email": "valid_email@example.com", "age_verification": "on"}
+        data = {"email": "valid_email@example.com"}
         response = client.post(url, data)
         messages = list(response.wsgi_request._messages)
         assert len([m for m in messages if m.tags == "success"]) == 1
@@ -14,7 +14,7 @@ class TestNewsletterFooter(TestCase):
     def test_newsletter_signup_fail(self):
         url = reverse("process_newsletter_form")
         client = Client()
-        data = {"email": "invalid_email", "age_verification": "on"}
+        data = {"email": "invalid_email"}
         response = client.post(url, data)
         messages = list(response.wsgi_request._messages)
         assert len([m for m in messages if "error" in m.tags]) == 1
