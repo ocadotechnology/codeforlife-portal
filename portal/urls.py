d6dac47da3981ddcd1c3698fff0e3db27d012c84
faucomte97 <florian.aucomt1@ocado.com>
2022-06-27 18:48:26 +0100

    feat: Add child-friendly legal documents

--- portal/urls.py
+++ portal/urls.py
@@ -1,32 +1,18 @@
 from aimmo.urls import HOMEPAGE_REGEX
 from common.permissions import teacher_verified
 from django.conf.urls import include, url
-from django.urls import reverse_lazy
 from django.views.generic import RedirectView
 from django.views.generic.base import TemplateView
 from django.views.i18n import JavaScriptCatalog
 from game.views.level import play_default_level
-from two_factor.views import (
-    BackupTokensView,
-    ProfileView,
-    QRGeneratorView,
-    SetupCompleteView,
-)
+from two_factor.views import BackupTokensView, ProfileView, QRGeneratorView, SetupCompleteView
 
 from portal.helpers.decorators import ratelimit
-from portal.helpers.ratelimit import (
-    RATELIMIT_LOGIN_GROUP,
-    RATELIMIT_METHOD,
-    RATELIMIT_LOGIN_RATE,
-)
+from portal.helpers.ratelimit import RATELIMIT_LOGIN_GROUP, RATELIMIT_METHOD, RATELIMIT_LOGIN_RATE
 from portal.helpers.regexes import ACCESS_CODE_REGEX
-
 from portal.reactTestSpace import reactTestSpace
 from portal.views.about import about, getinvolved, contribute
-from portal.views.admin import (
-    AdminChangePasswordDoneView,
-    AdminChangePasswordView,
-)
+from portal.views.admin import AdminChangePasswordDoneView, AdminChangePasswordView
 from portal.views.aimmo.dashboard import StudentAimmoDashboard, TeacherAimmoDashboard
 from portal.views.api import (
     AnonymiseOrphanSchoolsView,
@@ -37,27 +23,14 @@ from portal.views.api import (
 )
 from portal.views.dotmailer import dotmailer_consent_form, process_newsletter_form
 from portal.views.email import send_new_users_report, verify_email
-from portal.views.home import (
-    home,
-    home_learning,
-    logout_view,
-    register_view,
-)
+from portal.views.home import home, home_learning, logout_view, register_view
+from portal.views.legal import privacy_policy, terms
 from portal.views.login import old_login_form_redirect
 from portal.views.login.independent_student import IndependentStudentLoginView
-from portal.views.login.student import (
-    StudentLoginView,
-    StudentClassCodeView,
-    student_direct_login,
-)
+from portal.views.login.student import StudentLoginView, StudentClassCodeView, student_direct_login
 from portal.views.login.teacher import TeacherLoginView
-from portal.views.organisation import (
-    OrganisationFuzzyLookup,
-    organisation_leave,
-    organisation_manage,
-)
+from portal.views.organisation import OrganisationFuzzyLookup, organisation_leave, organisation_manage
 from portal.views.play_landing_page import play_landing_page
-from portal.views.privacy_policy import privacy_policy
 from portal.views.registration import (
     password_reset_check_and_confirm,
     password_reset_done,
@@ -65,17 +38,12 @@ from portal.views.registration import (
     teacher_password_reset,
     delete_account,
 )
-
 from portal.views.student.edit_account_details import (
     independentStudentEditAccountView,
     SchoolStudentEditAccountView,
     student_edit_account,
 )
-from portal.views.student.play import (
-    SchoolStudentDashboard,
-    IndependentStudentDashboard,
-    student_join_organisation,
-)
+from portal.views.student.play import SchoolStudentDashboard, IndependentStudentDashboard, student_join_organisation
 from portal.views.teach import teach
 from portal.views.teacher.dashboard import (
     dashboard_manage,
@@ -117,50 +85,19 @@ js_info_dict = {"packages": ("conf.locale",)}
 two_factor_patterns = [
     url(r"^account/two_factor/setup/$", CustomSetupView.as_view(), name="setup"),
     url(r"^account/two_factor/qrcode/$", QRGeneratorView.as_view(), name="qr"),
-    url(
-        r"^account/two_factor/setup/complete/$",
-        SetupCompleteView.as_view(),
-        name="setup_complete",
-    ),
-    url(
-        r"^account/two_factor/backup/tokens/$",
-        teacher_verified(BackupTokensView.as_view()),
-        name="backup_tokens",
-    ),
-    url(
-        r"^account/two_factor/$",
-        teacher_verified(ProfileView.as_view()),
-        name="profile",
-    ),
-    url(
-        r"^account/two_factor/disable/$",
-        teacher_verified(CustomDisableView.as_view()),
-        name="disable",
-    ),
+    url(r"^account/two_factor/setup/complete/$", SetupCompleteView.as_view(), name="setup_complete"),
+    url(r"^account/two_factor/backup/tokens/$", teacher_verified(BackupTokensView.as_view()), name="backup_tokens"),
+    url(r"^account/two_factor/$", teacher_verified(ProfileView.as_view()), name="profile"),
+    url(r"^account/two_factor/disable/$", teacher_verified(CustomDisableView.as_view()), name="disable"),
 ]
 
 
 urlpatterns = [
     url(HOMEPAGE_REGEX, include("aimmo.urls")),
-    url(
-        r"^teach/kurono/dashboard/$",
-        TeacherAimmoDashboard.as_view(),
-        name="teacher_aimmo_dashboard",
-    ),
-    url(
-        r"^play/kurono/dashboard/$",
-        StudentAimmoDashboard.as_view(),
-        name="student_aimmo_dashboard",
-    ),
-    url(
-        r"^favicon\.ico$",
-        RedirectView.as_view(url="/static/portal/img/favicon.ico", permanent=True),
-    ),
-    url(
-        r"^administration/password_change/$",
-        AdminChangePasswordView.as_view(),
-        name="administration_password_change",
-    ),
+    url(r"^teach/kurono/dashboard/$", TeacherAimmoDashboard.as_view(), name="teacher_aimmo_dashboard"),
+    url(r"^play/kurono/dashboard/$", StudentAimmoDashboard.as_view(), name="student_aimmo_dashboard"),
+    url(r"^favicon\.ico$", RedirectView.as_view(url="/static/portal/img/favicon.ico", permanent=True)),
+    url(r"^administration/password_change/$", AdminChangePasswordView.as_view(), name="administration_password_change"),
     url(
         r"^administration/password_change_done/$",
         AdminChangePasswordDoneView.as_view(),
@@ -168,11 +105,7 @@ urlpatterns = [
     ),
     url(r"^mail/weekly", send_new_users_report, name="send_new_users_report"),
     url(r"^users/inactive/", InactiveUsersView.as_view(), name="inactive_users"),
-    url(
-        r"^locked_out/$",
-        TemplateView.as_view(template_name="portal/locked_out.html"),
-        name="locked_out",
-    ),
+    url(r"^locked_out/$", TemplateView.as_view(template_name="portal/locked_out.html"), name="locked_out"),
     url(r"^", include((two_factor_patterns, "two_factor"), namespace="two_factor")),
     url(r"^i18n/", include("django.conf.urls.i18n")),
     url(r"^jsi18n/$", JavaScriptCatalog.as_view(), js_info_dict),
@@ -199,16 +132,8 @@ urlpatterns = [
         StudentLoginView.as_view(),
         name="student_login",
     ),
-    url(
-        r"^login/student/$",
-        StudentClassCodeView.as_view(),
-        name="student_login_access_code",
-    ),
-    url(
-        r"^u/(?P<user_id>[0-9]+)/(?P<login_id>[a-z0-9]+)/$",
-        student_direct_login,
-        name="student_direct_login",
-    ),
+    url(r"^login/student/$", StudentClassCodeView.as_view(), name="student_login_access_code"),
+    url(r"^u/(?P<user_id>[0-9]+)/(?P<login_id>[a-z0-9]+)/$", student_direct_login, name="student_direct_login"),
     url(
         r"^login/independent/$",
         ratelimit(
@@ -231,21 +156,9 @@ urlpatterns = [
         name="email_verification",
     ),
     url(r"^verify_email/(?P<token>[0-9a-f]+)/$", verify_email, name="verify_email"),
-    url(
-        r"^user/password/reset/student/$",
-        student_password_reset,
-        name="student_password_reset",
-    ),
-    url(
-        r"^user/password/reset/teacher/$",
-        teacher_password_reset,
-        name="teacher_password_reset",
-    ),
-    url(
-        r"^user/password/reset/done/$",
-        password_reset_done,
-        name="reset_password_email_sent",
-    ),
+    url(r"^user/password/reset/student/$", student_password_reset, name="student_password_reset"),
+    url(r"^user/password/reset/teacher/$", teacher_password_reset, name="teacher_password_reset"),
+    url(r"^user/password/reset/done/$", password_reset_done, name="reset_password_email_sent"),
     url(
         r"^user/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$",
         password_reset_check_and_confirm,
@@ -257,21 +170,9 @@ urlpatterns = [
         name="password_reset_complete",
     ),
     url(r"^teach/$", teach, name="teach"),
-    url(
-        r"^teach/fuzzy_lookup/$",
-        OrganisationFuzzyLookup.as_view(),
-        name="organisation_fuzzy_lookup",
-    ),
-    url(
-        r"^teach/onboarding-organisation/$",
-        organisation_manage,
-        name="onboarding-organisation",
-    ),
-    url(
-        r"^teach/onboarding-classes",
-        teacher_onboarding_create_class,
-        name="onboarding-classes",
-    ),
+    url(r"^teach/fuzzy_lookup/$", OrganisationFuzzyLookup.as_view(), name="organisation_fuzzy_lookup"),
+    url(r"^teach/onboarding-organisation/$", organisation_manage, name="onboarding-organisation"),
+    url(r"^teach/onboarding-classes", teacher_onboarding_create_class, name="onboarding-classes"),
     url(
         rf"^teach/onboarding-class/(?P<access_code>{ACCESS_CODE_REGEX})$",
         teacher_onboarding_edit_class,
@@ -290,11 +191,7 @@ urlpatterns = [
     url(r"^teach/invite", invite_teacher, name="invite_teacher"),
     url(r"^play/$", play_landing_page, name="play"),
     url(r"^play/details/$", SchoolStudentDashboard.as_view(), name="student_details"),
-    url(
-        r"^play/details/independent$",
-        IndependentStudentDashboard.as_view(),
-        name="independent_student_details",
-    ),
+    url(r"^play/details/independent$", IndependentStudentDashboard.as_view(), name="independent_student_details"),
     url(r"^play/account/$", student_edit_account, name="student_edit_account"),
     url(
         r"^play/account/independent/$",
@@ -308,56 +205,24 @@ urlpatterns = [
         )(independentStudentEditAccountView),
         name="independent_edit_account",
     ),
-    url(
-        r"^play/account/school_student/$",
-        SchoolStudentEditAccountView.as_view(),
-        name="school_student_edit_account",
-    ),
+    url(r"^play/account/school_student/$", SchoolStudentEditAccountView.as_view(), name="school_student_edit_account"),
     url(r"^play/join/$", student_join_organisation, name="student_join_organisation"),
     url(r"^about", about, name="about"),
     url(r"^getinvolved", getinvolved, name="getinvolved"),
     url(r"^contribute", contribute, name="contribute"),
-    url(r"^terms", TemplateView.as_view(template_name="portal/terms.html"), name="terms"),
+    url(r"^terms", terms, name="terms"),
     url(r"^privacy-policy/$", privacy_policy, name="privacy_policy"),
     url(r"^teach/materials/$", materials, name="materials"),
     url(r"^teach/kurono_teaching_packs$", kurono_teaching_packs, name="kurono_packs"),
     url(r"^teach/resources/$", teacher_rapid_router_resources, name="teaching_resources"),
-    url(
-        r"^teach/kurono_resources/$",
-        teacher_kurono_resources,
-        name="kurono_teaching_resources",
-    ),
+    url(r"^teach/kurono_resources/$", teacher_kurono_resources, name="kurono_teaching_resources"),
     url(r"^teach/dashboard/$", dashboard_manage, name="dashboard"),
-    url(
-        r"^teach/dashboard/kick/(?P<pk>[0-9]+)/$",
-        organisation_kick,
-        name="organisation_kick",
-    ),
-    url(
-        r"^teach/dashboard/toggle_admin/(?P<pk>[0-9]+)/$",
-        organisation_toggle_admin,
-        name="organisation_toggle_admin",
-    ),
-    url(
-        r"^teach/dashboard/disable_2FA/(?P<pk>[0-9]+)/$",
-        teacher_disable_2FA,
-        name="teacher_disable_2FA",
-    ),
-    url(
-        r"^teach/dashboard/allow_join/(?P<pk>[0-9]+)/$",
-        organisation_allow_join,
-        name="organisation_allow_join",
-    ),
-    url(
-        r"^teach/dashboard/deny_join/(?P<pk>[0-9]+)/$",
-        organisation_deny_join,
-        name="organisation_deny_join",
-    ),
-    url(
-        r"^teach/dashboard/school/leave/$",
-        organisation_leave,
-        name="organisation_leave",
-    ),
+    url(r"^teach/dashboard/kick/(?P<pk>[0-9]+)/$", organisation_kick, name="organisation_kick"),
+    url(r"^teach/dashboard/toggle_admin/(?P<pk>[0-9]+)/$", organisation_toggle_admin, name="organisation_toggle_admin"),
+    url(r"^teach/dashboard/disable_2FA/(?P<pk>[0-9]+)/$", teacher_disable_2FA, name="teacher_disable_2FA"),
+    url(r"^teach/dashboard/allow_join/(?P<pk>[0-9]+)/$", organisation_allow_join, name="organisation_allow_join"),
+    url(r"^teach/dashboard/deny_join/(?P<pk>[0-9]+)/$", organisation_deny_join, name="organisation_deny_join"),
+    url(r"^teach/dashboard/school/leave/$", organisation_leave, name="organisation_leave"),
     url(
         r"^teach/dashboard/student/accept/(?P<pk>[0-9]+)/$",
         teacher_accept_student_request,
@@ -368,31 +233,17 @@ urlpatterns = [
         teacher_reject_student_request,
         name="teacher_reject_student_request",
     ),
+    url(rf"^teach/class/(?P<access_code>{ACCESS_CODE_REGEX})$", teacher_view_class, name="view_class"),
     url(
-        rf"^teach/class/(?P<access_code>{ACCESS_CODE_REGEX})$",
-        teacher_view_class,
-        name="view_class",
-    ),
-    url(
-        rf"^teach/class/delete/(?P<access_code>{ACCESS_CODE_REGEX})$",
-        teacher_delete_class,
-        name="teacher_delete_class",
+        rf"^teach/class/delete/(?P<access_code>{ACCESS_CODE_REGEX})$", teacher_delete_class, name="teacher_delete_class"
     ),
     url(
         rf"^teach/class/(?P<access_code>{ACCESS_CODE_REGEX})/students/delete/$",
         teacher_delete_students,
         name="teacher_delete_students",
     ),
-    url(
-        rf"^teach/class/edit/(?P<access_code>{ACCESS_CODE_REGEX})$",
-        teacher_edit_class,
-        name="teacher_edit_class",
-    ),
-    url(
-        r"^teach/class/student/edit/(?P<pk>[0-9]+)/$",
-        teacher_edit_student,
-        name="teacher_edit_student",
-    ),
+    url(rf"^teach/class/edit/(?P<access_code>{ACCESS_CODE_REGEX})$", teacher_edit_class, name="teacher_edit_class"),
+    url(r"^teach/class/student/edit/(?P<pk>[0-9]+)/$", teacher_edit_student, name="teacher_edit_student"),
     url(
         rf"^teach/class/(?P<access_code>{ACCESS_CODE_REGEX})/password_reset/$",
         teacher_class_password_reset,
--- portal/views/privacy_policy.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def privacy_policy(request):
-    return render(
-        request,
-        "portal/privacy_policy.html",
-        {"last_updated": "3rd September 2021"},
-    )
