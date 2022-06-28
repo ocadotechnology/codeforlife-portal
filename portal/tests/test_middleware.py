532198e2311f03e63c52d9df3bffd8b8dc34fff6
Razvan Mahu <67904187+razvan-pro@users.noreply.github.com>
2022-06-28 15:53:01 +0100

    feat: Screentime warning (#1888)
    * wip: add screentime popup
    
    * add ScreentimeWarningMiddleware and reset url
    
    * reset screentime warning
    
    * add brain and test
    
    * Merge branch 'master' into screentime-warning
    
    * fix test
    
    * test reset_screentime_warning API
    
    * remove weird comment

--- portal/tests/test_middleware.py
+++ portal/tests/test_middleware.py
@@ -1,5 +1,7 @@
 import time
+from typing import Tuple
 from unittest import mock
+
 from _pytest.monkeypatch import MonkeyPatch
 from common.tests.utils.classes import create_class_directly
 from common.tests.utils.organisation import create_organisation_directly
@@ -9,6 +11,7 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.test import Client, TestCase
+from django.urls import reverse
 
 MOCKED_SESSION_EXPIRY_TIME = 5
 
@@ -31,7 +34,7 @@ class TestAdminAccessMiddleware(TestCase):
         self.monkeypatch = MonkeyPatch()
         self.monkeypatch.setattr("deploy.middleware.admin_access.MODULE_NAME", "test")
 
-    def _setup_user(self) -> (str, str):
+    def _setup_user(self) -> Tuple[str, str]:
         email, password = signup_teacher_directly()
         create_organisation_directly(email)
         _, _, access_code = create_class_directly(email)
@@ -137,11 +140,9 @@ class TestSessionTimeoutMiddleware(TestCase):
         self.email, self.password = self._setup_user()
 
         self.monkeypatch = MonkeyPatch()
-        self.monkeypatch.setattr(
-            "deploy.middleware.session_timeout.SESSION_EXPIRY_TIME", 5
-        )
+        self.monkeypatch.setattr("deploy.middleware.session_timeout.SESSION_EXPIRY_TIME", 5)
 
-    def _setup_user(self) -> (str, str):
+    def _setup_user(self) -> Tuple[str, str]:
         email, password = signup_teacher_directly()
         create_organisation_directly(email)
         _, _, access_code = create_class_directly(email)
@@ -163,3 +164,51 @@ class TestSessionTimeoutMiddleware(TestCase):
         user = auth.get_user(self.client)
 
         assert not user.is_authenticated
+
+
+class TestScreentimeWarningMiddleware(TestCase):
+    """
+    This tests the ScreentimeWarningMiddleware class and popup timeout is set properly.
+    """
+
+    def setUp(self) -> None:
+        self.client = Client()
+        self.email, self.password = self._setup_user()
+
+    def _setup_user(self) -> Tuple[str, str]:
+        email, password = signup_teacher_directly()
+        create_organisation_directly(email)
+        _, _, access_code = create_class_directly(email)
+        create_school_student_directly(access_code)
+
+        return email, password
+
+    def test_screentime_warning_timeout(self):
+        # Timeout should not be there if the user is not logged in
+        session = self.client.session
+        assert "screentime_warning_timeout" not in session
+
+        # Log in as a teacher
+        self.client.login(username=self.email, password=self.password)
+
+        # Check the screentime_warning_timeout decreases after consecutive requests
+        self.client.get("/")
+        session = self.client.session
+        assert "screentime_warning_timeout" in session
+        previous_screentime_warning_timeout = session["screentime_warning_timeout"]
+
+        self.client.get("/")
+        session = self.client.session
+        assert "screentime_warning_timeout" in session
+        new_screentime_warning_timeout = session["screentime_warning_timeout"]
+
+        assert new_screentime_warning_timeout < previous_screentime_warning_timeout
+
+        # Check the reset_screentime_warning API resets the timeout
+        url = reverse("reset_screentime_warning")
+        self.client.get(url)
+        self.client.get("/")
+        session = self.client.session
+        assert "screentime_warning_timeout" in session
+        renewed_screentime_warning_timeout = session["screentime_warning_timeout"]
+        assert renewed_screentime_warning_timeout > new_screentime_warning_timeout
