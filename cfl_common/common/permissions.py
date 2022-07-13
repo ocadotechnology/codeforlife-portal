18b1830637aaab64f771ffe38c5bb0d108ff981a
KamilPawel <kamil.sosinski@ocado.com>
2022-07-13 14:13:00 +0100

    fix one bug at a time #20

--- cfl_common/common/permissions.py
+++ cfl_common/common/permissions.py
@@ -46,7 +46,7 @@ def check_teacher_authorised(request, teacher):
     current_teacher_owns_the_class = teacher == request.user.new_teacher
     is_current_teacher_school_admin = teacher.school == request.user.new_teacher.school and teacher.is_admin
 
-    if not (current_teacher_owns_the_class and is_current_teacher_school_admin):
+    if not (current_teacher_owns_the_class or is_current_teacher_school_admin):
         raise Http404
 
 
