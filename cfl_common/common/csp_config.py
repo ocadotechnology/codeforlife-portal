d7e489160dd3b72e274a6409fbfedf0caa93d720
faucomte97 <florian.aucomt1@ocado.com>
2022-09-02 15:56:38 +0100

    Clean CSP

--- cfl_common/common/csp_config.py
+++ cfl_common/common/csp_config.py
@@ -13,9 +13,9 @@ CSP_CONNECT_SRC = (
     "https://api.unisvg.com/",
     "https://www.google-analytics.com/",
     "https://pyodide-cdn2.iodide.io/v0.15.0/full/",
+    "https://crowdin.com/",
     f"wss://{MODULE_NAME}-aimmo.codeforlife.education/",
     f"https://{MODULE_NAME}-aimmo.codeforlife.education/",
-    "https://crowdin.com/",
 )
 CSP_FONT_SRC = ("'self'", "https://fonts.gstatic.com/", "https://fonts.googleapis.com/", "https://use.typekit.net/")
 CSP_SCRIPT_SRC = (
@@ -54,28 +54,28 @@ CSP_FRAME_SRC = (
     "https://www.youtube-nocookie.com/",
     "https://www.recaptcha.net/",
     "https://www.google.com/recaptcha/",
+    "https://crowdin.com/",
     f"{domain()}/static/common/img/",
     f"{domain()}/static/game/image/",
-    "https://crowdin.com/",
 )
 CSP_IMG_SRC = (
     "https://storage.googleapis.com/codeforlife-assets/images/",
     "https://cdn-ukwest.onetrust.com/",
+    "https://p.typekit.net/",
+    "https://cdn.crowdin.com/",
+    "https://crowdin-static.downloads.crowdin.com/",
+    "data:",
     f"{domain()}/static/portal/img/",
-    f"{domain()}/static/portal/static/portal/img/",  # temo1
-    f"{domain()}/static/portal/img/",  # temo2
+    f"{domain()}/static/portal/static/portal/img/",
+    f"{domain()}/static/portal/img/",
     f"{domain()}/favicon.ico",
     f"{domain()}/img/",
     f"{domain()}/account/two_factor/qrcode/",
     f"{domain()}/static/",
-    "https://p.typekit.net/",
     f"{domain()}/static/game/image/",
     f"{domain()}/static/game/raphael_image/",
     f"{domain()}/static/game/js/blockly/media/",
     f"{domain()}/static/icons/",
-    "https://cdn.crowdin.com/",
-    "https://crowdin-static.downloads.crowdin.com/",
-    "data:",
 )
 CSP_OBJECT_SRC = (f"{domain()}/static/common/img/", f"{domain()}/static/game/image/")
 CSP_MEDIA_SRC = (f"{domain()}/static/game/sound/", f"{domain()}/static/game/js/blockly/media/")
