c8fb6bdcf69e8a5b486175325782fc9dd086f3c7
faucomte97 <florian.aucomt1@ocado.com>
2022-09-20 19:49:53 +0100

    fix: Make kurono maintenance banner show depending on flag

--- portal/views/home.py
+++ portal/views/home.py
@@ -230,19 +230,23 @@ def redirect_teacher_to_correct_page(request, teacher):
 
 @cache_control(private=True)
 def home(request):
+    kurono_down = False
+
     class_tag = "freshdesk__contact-us"
-    messages.info(
-        request,
-        format_html(
-            "We are currently carrying out some maintenance work on "
-            + "our website which means Kurono is temporarily unavailable. "
-            + "We will have everything up and running again as soon as possible. "
-            + "Please <a class='"
-            + class_tag
-            + "'>contact us</a> if you have any questions."
-        ),
-        extra_tags="safe",
-    )
+
+    if kurono_down:
+        messages.info(
+            request,
+            format_html(
+                "We are currently carrying out some maintenance work on "
+                + "our website which means Kurono is temporarily unavailable. "
+                + "We will have everything up and running again as soon as possible. "
+                + "Please <a class='"
+                + class_tag
+                + "'>contact us</a> if you have any questions."
+            ),
+            extra_tags="safe",
+        )
 
     """
     This view is where we can add any messages to be shown upon loading the home page.
