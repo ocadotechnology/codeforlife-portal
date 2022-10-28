4b6711381bf23867af050af69a667df52ddf2b14
KamilPawel <kamil.sosinski@ocado.com>
2022-10-28 10:06:39 +0100

    dummy commit

--- portal/forms/registration.py
+++ portal/forms/registration.py
@@ -10,7 +10,6 @@ from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
-
 from portal.helpers.password import PasswordStrength, form_clean_password
 
 
