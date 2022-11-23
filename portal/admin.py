0076abb66c2f35097d93048e4e8536e09da027b6
Florian Aucomte <33633200+faucomte97@users.noreply.github.com>
2022-11-23 13:04:48 +0000

    fix: Improve admin panel usability (#1973)

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,5 @@
+import csv
+
 from common.models import (
     Class,
     EmailVerification,
@@ -7,24 +9,22 @@ from common.models import (
     Teacher,
     UserProfile,
     DynamicElement,
+    DailyActivity,
 )
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+from import_export.admin import ExportActionMixin
 
 from portal.forms.admin import AdminChangeUserPasswordForm, AdminUserCreationForm
 from portal.views.api import anonymise
-
-from import_export.admin import ExportActionMixin
-
-import csv
-from django.http import HttpResponse
 
 
 class ClassAdmin(admin.ModelAdmin, ExportActionMixin):
     search_fields = ["name", "teacher__new_user__first_name", "teacher__new_user__last_name", "teacher__school__name"]
     list_display = ["__str__", "teacher", "teacher_school"]
-    raw_id_fields = ["teacher"]
+    autocomplete_fields = ["teacher"]
 
     def teacher_school(self, obj):
         return obj.teacher.school
@@ -33,20 +33,28 @@ class ClassAdmin(admin.ModelAdmin, ExportActionMixin):
 class SchoolAdmin(admin.ModelAdmin, ExportActionMixin):
     search_fields = ["name", "country", "postcode", "town"]
     list_filter = ["postcode", "country"]
+    list_display = ["__str__", "postcode", "country", "number_of_teachers", "number_of_classes"]
+
+    def number_of_teachers(self, obj):
+        return len(obj.teacher_school.all())
+
+    def number_of_classes(self, obj):
+        return len(obj.classes())
 
 
 class StudentAdmin(admin.ModelAdmin, ExportActionMixin):
     search_fields = [
         "new_user__first_name",
         "new_user__last_name",
+        "new_user__username",
         "class_field__name",
         "class_field__teacher__new_user__first_name",
         "class_field__teacher__new_user__last_name",
         "class_field__teacher__school__name",
     ]
-    list_display = ["__str__", "class_field", "class_field_teacher", "class_field_school"]
-    readonly_fields = ["user", "new_user"]
-    raw_id_fields = ["class_field", "pending_class_request"]
+    list_display = ["__str__", "class_field", "class_field_teacher", "class_field_school", "new_user"]
+    readonly_fields = ["user", "new_user", "login_id"]
+    autocomplete_fields = ["class_field", "pending_class_request"]
 
     def class_field_teacher(self, obj):
         if obj.class_field:
@@ -62,21 +70,22 @@ class StudentAdmin(admin.ModelAdmin, ExportActionMixin):
 
 
 class TeacherAdmin(admin.ModelAdmin, ExportActionMixin):
-    search_fields = ["new_user__first_name", "new_user__last_name", "school__name"]
-    list_display = ["__str__", "school"]
+    search_fields = ["new_user__first_name", "new_user__last_name", "school__name", "new_user__username"]
+    list_display = ["__str__", "school", "new_user", "is_admin"]
     readonly_fields = ["user", "new_user"]
-    raw_id_fields = ["school"]
+    autocomplete_fields = ["school", "invited_by"]
 
 
 class UserProfileAdmin(admin.ModelAdmin, ExportActionMixin):
     search_fields = ["user__first_name", "user__last_name", "user__username", "user__date_joined"]
     list_filter = ["user__date_joined"]
-    list_display = ["user", "__str__", "joined_recently"]
+    list_display = ["user", "__str__"]
     readonly_fields = ["user"]
 
 
 class EmailVerificationAdmin(admin.ModelAdmin, ExportActionMixin):
     search_fields = ["user__first_name", "user__last_name", "user__username", "user__date_joined"]
+    list_display = ["__str__", "user", "verified"]
     readonly_fields = ["user", "token"]
 
 
@@ -116,7 +125,7 @@ def export_as_csv(self, request, queryset):
 
     writer.writerow(field_names)
     for obj in queryset:
-        row = writer.writerow([getattr(obj, field) for field in field_names])
+        writer.writerow([getattr(obj, field) for field in field_names])
 
     return response
 
@@ -143,3 +152,4 @@ admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(EmailVerification, EmailVerificationAdmin)
 admin.site.register(SchoolTeacherInvitation, SchoolTeacherInvitationAdmin)
 admin.site.register(DynamicElement, DynamicElementAdmin)
+admin.site.register(DailyActivity)
