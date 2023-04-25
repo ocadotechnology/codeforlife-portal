549cd83d1c07645e01f1e698e92a0e7744f716c5
faucomte97 <f.aucomte@hotmail.co.uk>
2023-04-25 11:31:01 +0100

    Add revert code to JWT migration

--- cfl_common/common/migrations/0037_migrate_email_verification.py
+++ cfl_common/common/migrations/0037_migrate_email_verification.py
@@ -7,10 +7,19 @@ class Migration(migrations.Migration):
     ]
 
     def forwards(apps, schema_editor):
+        """ Finds the users of verified Email Verification objects and sets their `is_verified` to True """
         EmailVerification = apps.get_model("common", "EmailVerification")
         UserProfile = apps.get_model("common", "UserProfile")
         db_alias = schema_editor.connection.alias
         for email_verification in EmailVerification.objects.using(db_alias).filter(verified=True):
             UserProfile.objects.using(db_alias).filter(user=email_verification.user).update(is_verified=True)
 
-    operations = [migrations.RunPython(forwards)]
+    def backwards(apps, schema_editor):
+        """ Finds the users of verified Email Verification objects and sets their `is_verified` to False """
+        EmailVerification = apps.get_model("common", "EmailVerification")
+        UserProfile = apps.get_model("common", "UserProfile")
+        db_alias = schema_editor.connection.alias
+        for email_verification in EmailVerification.objects.using(db_alias).filter(verified=True):
+            UserProfile.objects.using(db_alias).filter(user=email_verification.user).update(is_verified=False)
+
+    operations = [migrations.RunPython(forwards, reverse_code=backwards)]
