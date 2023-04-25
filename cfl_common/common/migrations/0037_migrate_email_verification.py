b865eeb03a14b7e6f069c981272fd5e0782ae5a7
faucomte97 <f.aucomte@hotmail.co.uk>
2023-04-25 12:49:33 +0100

    Improve query in migration 0037

--- cfl_common/common/migrations/0037_migrate_email_verification.py
+++ cfl_common/common/migrations/0037_migrate_email_verification.py
@@ -8,18 +8,14 @@ class Migration(migrations.Migration):
 
     def forwards(apps, schema_editor):
         """ Finds the users of verified Email Verification objects and sets their `is_verified` to True """
-        EmailVerification = apps.get_model("common", "EmailVerification")
         UserProfile = apps.get_model("common", "UserProfile")
         db_alias = schema_editor.connection.alias
-        for email_verification in EmailVerification.objects.using(db_alias).filter(verified=True):
-            UserProfile.objects.using(db_alias).filter(user=email_verification.user).update(is_verified=True)
+        UserProfile.objects.using(db_alias).filter(user__email_verifications__verified=True).update(is_verified=True)
 
     def backwards(apps, schema_editor):
         """ Finds the users of verified Email Verification objects and sets their `is_verified` to False """
-        EmailVerification = apps.get_model("common", "EmailVerification")
         UserProfile = apps.get_model("common", "UserProfile")
         db_alias = schema_editor.connection.alias
-        for email_verification in EmailVerification.objects.using(db_alias).filter(verified=True):
-            UserProfile.objects.using(db_alias).filter(user=email_verification.user).update(is_verified=False)
+        UserProfile.objects.using(db_alias).filter(user__email_verifications__verified=True).update(is_verified=False)
 
     operations = [migrations.RunPython(forwards, reverse_code=backwards)]
