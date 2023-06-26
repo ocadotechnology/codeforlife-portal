b9cde0c47cd86f53f970488c356539dfaa50d70c
KamilPawel <ks6g18@soton.ac.uk>
2023-06-26 17:39:30 +0100

    remove sample test

--- portal/tests/test_helper_methods.py
+++ portal/tests/test_helper_methods.py
@@ -6,10 +6,3 @@ def test_is_password_pwned():
     strong_password = "£EDCVFR$%TGBnhy667ujm"
     assert is_password_pwned(weak_password)
     assert not is_password_pwned(strong_password)
-
-
-def test_api_is_password_pwned():
-    weak_password = "Password123$"
-    strong_password = "£EDCVFR$%TGBnhy667ujm"
-    assert is_password_pwned(weak_password).status_code == 200
-    assert not is_password_pwned(strong_password).status_code == 200
