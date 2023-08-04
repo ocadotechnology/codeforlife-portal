ceed8a8cbf61e9151858e6c047f8819b0f07fdb5
faucomte97 <f.aucomte@hotmail.co.uk>
2023-08-04 17:49:47 +0100

    Refactor tests

--- portal/tests/test_helper_methods.py
+++ portal/tests/test_helper_methods.py
@@ -12,6 +12,24 @@ class TestClass:
         assert is_password_pwned(weak_password)
         assert not is_password_pwned(strong_password)
 
+    @patch("requests.get")
+    def test_is_password_pwned_status_code_not_200(self, mock_get):
+        # Arrange
+        password = "password123"
+        sha1_hash = hashlib.sha1(password.encode()).hexdigest()
+
+        mock_response = Mock()
+        mock_response.status_code = 500
+
+        mock_get.return_value = mock_response
+
+        # Act
+        result = is_password_pwned(password)
+
+        # Assert
+        mock_get.assert_called_once_with(f"https://api.pwnedpasswords.com/range/{sha1_hash[:5]}")
+        assert not result
+
     def test_sanitise_uk_postcode(self):
         postcode_with_space = "AL10 9NE"
         postcode_without_space = "AL109UL"
@@ -20,42 +38,3 @@ class TestClass:
         assert sanitise_uk_postcode(postcode_with_space) == "AL10 9NE"  # Check it stays the same
         assert sanitise_uk_postcode(postcode_without_space) == "AL10 9UL"  # Check a space is added
         assert sanitise_uk_postcode(invalid_postcode) == "123"  # Check nothing happens
-
-
-@patch("requests.get")
-def test_is_password_pwned(mock_get):
-    # Arrange
-    password = "password123"
-    sha1_hash = hashlib.sha1(password.encode()).hexdigest()
-
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_response.text = f"{sha1_hash[5:]}\r\n"
-
-    mock_get.return_value = mock_response
-
-    # Act
-    result = is_password_pwned(password)
-
-    # Assert
-    mock_get.assert_called_once_with(f"https://api.pwnedpasswords.com/range/{sha1_hash[:5]}")
-    assert result
-
-
-@patch("requests.get")
-def test_is_password_pwned_status_code_not_200(mock_get):
-    # Arrange
-    password = "password123"
-    sha1_hash = hashlib.sha1(password.encode()).hexdigest()
-
-    mock_response = Mock()
-    mock_response.status_code = 500
-
-    mock_get.return_value = mock_response
-
-    # Act
-    result = is_password_pwned(password)
-
-    # Assert
-    mock_get.assert_called_once_with(f"https://api.pwnedpasswords.com/range/{sha1_hash[:5]}")
-    assert not result
