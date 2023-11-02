efb9ccacef4ae52c1a5e525f6f4c24ac555b15a6
SKairinos <skairinos@gmail.com>
2023-11-02 16:48:07 +0000

    teacher account

--- portal/static/portal/js/organisation_manage.js
+++ portal/static/portal/js/organisation_manage.js
@@ -89,3 +89,27 @@ function showDeleteAccountConfirmation(delete_password, unsubscribe_newsletter, 
 function hideDeleteAccountPopup() {
   $("#popup-delete-review").removeClass("popup--fade");
 }
+
+$(document).ready(() => {
+  $('#edit_account_details_password').on(
+    'click',
+    '#password-field-icon, #confirm-password-field-icon',
+    () => {
+      let inputType;
+      let dataIcon = $('#password-field-icon').attr('data-icon');
+      if (dataIcon === 'material-symbols:visibility') {
+        inputType = 'password';
+        dataIcon = 'material-symbols:visibility-off';
+      } else {
+        inputType = 'text';
+        dataIcon = 'material-symbols:visibility';
+      }
+
+      $('#id_password').attr('type', inputType);
+      $('#id_confirm_password').attr('type', inputType);
+      
+      $('#password-field-icon').attr('data-icon', dataIcon);
+      $('#confirm-password-field-icon').attr('data-icon', dataIcon);
+    }
+  );
+});
