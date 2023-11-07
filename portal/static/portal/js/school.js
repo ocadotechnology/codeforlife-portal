438a97b6f32e28a6897de19991662a375452fb58
SKairinos <skairinos@gmail.com>
2023-11-07 16:51:36 +0000

    show/hide county field pt.2

--- portal/static/portal/js/school.js
+++ portal/static/portal/js/school.js
@@ -1,8 +1,10 @@
 $(document).ready(() => {
-  $('#form-row-county').hide();
+  if ($("#id_country").val() !== 'GB') {
+    $('#form-row-county').hide();
+  }
   
   $('#id_country').on('change', (event) => {
-    if (event.target.value == 'GB') {
+    if (event.target.value === 'GB') {
       $('#form-row-county').show();
     } else {
       $('#form-row-county').hide();
