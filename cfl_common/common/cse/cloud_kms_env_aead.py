c5b0ed61035c2a3f8c2f81ebd1e58cef4c06e14a
faucomte97 <f.aucomte@hotmail.co.uk>
2024-04-03 13:35:37 +0100

    Extend client class

--- cfl_common/common/cse/cloud_kms_env_aead.py
+++ cfl_common/common/cse/cloud_kms_env_aead.py
@@ -1,8 +1,10 @@
 import logging
 
 import tink
+from google.cloud import kms_v1
+from google.oauth2 import service_account
 from tink import aead
-from tink.integration import gcpkms
+from tink.integration.gcpkms import GcpKmsClient as _GcpKmsClient
 
 logger = logging.getLogger(__name__)
 
@@ -16,7 +18,7 @@ def init_tink_env_aead(
     aead.register()
 
     try:
-        gcp_client = gcpkms.GcpKmsClient(key_uri, credentials)
+        gcp_client = GcpKmsClient(key_uri, credentials)
         gcp_aead = gcp_client.get_aead(key_uri)
     except tink.TinkError as e:
         logger.error("Error initializing GCP client: %s", e)
@@ -31,3 +33,36 @@ def init_tink_env_aead(
     print(f"Created envelope AEAD Primitive using KMS URI: {key_uri}")
 
     return env_aead
+
+
+class GcpKmsClient(_GcpKmsClient):
+    """Basic GCP client for AEAD."""
+
+    def __init__(
+        self, key_uri: str, credentials_json: str
+    ) -> None:
+        """Creates a new GcpKmsClient that is bound to the key specified in 'key_uri'.
+
+        Uses the specified credentials when communicating with the KMS.
+
+        Args:
+          key_uri: The URI of the key the client should be bound to. If it is None
+              or empty, then the client is not bound to any particular key.
+          credentials_json: Path to the file with the access credentials. If it is
+              None or empty, then default credentials will be used.
+
+        Raises:
+          ValueError: If the path or filename of the credentials is invalid.
+          TinkError: If the key uri is not valid.
+        """
+        if key_uri.startswith("gcp-kms://"):
+            self._key_uri = key_uri
+        else:
+            raise tink.TinkError("Invalid key_uri.")
+
+        credentials = service_account.Credentials.from_service_account_info(
+            credentials_json
+        )
+        self._client = kms_v1.KeyManagementServiceClient(
+            credentials=credentials
+        )
