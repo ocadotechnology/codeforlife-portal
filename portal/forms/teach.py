cbf4be77d237478f5bcd1eda8cea464f6be42c0d
besque <gaganrh717@gmail.com>
2024-10-25 16:01:08 +0530

    fix: Update id generation while maintaining field names

--- portal/forms/teach.py
+++ portal/forms/teach.py
@@ -19,6 +19,12 @@ from portal.helpers.ratelimit import clear_ratelimit_cache_for_user
 
 class InvitedTeacherForm(forms.Form):
     prefix = 'teacher_signup'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['id'] = f'id_teacher_signup-{field.name}'
+
     teacher_password = forms.CharField(
         help_text="Enter a password",
         widget=forms.PasswordInput(
