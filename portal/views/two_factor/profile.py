d637a13a045bc5e408f83b40707122d51b3f1909
faucomte97 <f.aucomte@hotmail.co.uk>
2024-11-05 20:09:23 +0000

    Fix some 2fa stuff

--- portal/views/two_factor/profile.py
+++ portal/views/two_factor/profile.py
@@ -1,11 +1,11 @@
+from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from two_factor.views.profile import DisableView
-from two_factor.views.utils import class_view_decorator
 
 from .form import DisableForm
 
 
 # This is not changed but imports the from form.py so it overwrites the disable 2FA form
-@class_view_decorator(never_cache)
+@method_decorator(never_cache, name='dispatch')
 class CustomDisableView(DisableView):
     form_class = DisableForm
