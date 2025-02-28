4351efa919238d6535a14351aea549c9c0cc147d
Florian Aucomte <f.aucomte@hotmail.co.uk>
2025-02-28 16:46:47 +0000

    fix: Use Gitbook videos instead of YouTube links

--- cfl_common/common/csp_config.py
+++ cfl_common/common/csp_config.py
@@ -49,7 +49,8 @@ CSP_STYLE_SRC = (
 )
 CSP_FRAME_SRC = (
     "https://storage.googleapis.com/",
-    "https://2662351606-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/"
+    "https://2662351606-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/",
+    "https://files.gitbook.com/v0/b/gitbook-x-prod.appspot.com/",
     "https://www.youtube-nocookie.com/",
     "https://www.recaptcha.net/",
     "https://www.google.com/recaptcha/",
