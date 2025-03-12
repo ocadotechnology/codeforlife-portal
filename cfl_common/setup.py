5ae607e861366999488bf6c1a3b85a20abba03b2
Florian Aucomte <f.aucomte@hotmail.co.uk>
2025-03-12 11:08:04 +0000

    Move all deps to Pipfiles

--- cfl_common/setup.py
+++ cfl_common/setup.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
+import json
+import typing as t
 
 from setuptools import find_packages, setup
 
@@ -10,25 +12,44 @@ with open("../portal/__init__.py", "r") as fd:
         re.MULTILINE,
     ).group(1)
 
+def parse_requirements(packages: t.Dict[str, t.Dict[str, t.Any]]):
+    """Parse a group of requirements from `Pipfile.lock`.
+
+    https://setuptools.pypa.io/en/latest/userguide/dependency_management.html
+
+    Args:
+        packages: The group name of the requirements.
+
+    Returns:
+        The requirements as a list of strings, required by `setuptools.setup`.
+    """
+
+    requirements: t.List[str] = []
+    for name, package in packages.items():
+        requirement = name
+        if "extras" in package:
+            requirement += f"[{','.join(package['extras'])}]"
+        if "version" in package:
+            requirement += package["version"]
+        if "markers" in package:
+            requirement += f"; {package['markers']}"
+        requirements.append(requirement)
+
+    return requirements
+
+# Parse Pipfile.lock into strings.
+with open("Pipfile.lock", "r", encoding="utf-8") as pipfile_lock:
+    lock = json.load(pipfile_lock)
+    install_requires = parse_requirements(lock["default"])
+    dev_requires = parse_requirements(lock["develop"])
+
 setup(
     name="cfl-common",
     long_description="Common package for Code for Life",
     packages=find_packages(),
     version=version,
     include_package_data=True,
-    install_requires=[
-        "django==4.2.20",
-        "django-countries==7.6.1",
-        "django-csp==3.8",
-        "django-import-export==4.2.0",
-        "django-pipeline==3.1.0",
-        "django-two-factor-auth==1.17.0",
-        "djangorestframework==3.15.2",
-        "libsass==0.23.0",
-        "more-itertools==8.7.0",
-        "pgeocode==0.4.0",
-        "pyjwt==2.6.0",
-    ],
+    install_requires=install_requires,
     tests_require=[],
     test_suite="tests",
     classifiers=[
