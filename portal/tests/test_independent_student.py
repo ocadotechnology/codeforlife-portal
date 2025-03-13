4b7618df154ef827a9ee3aeca615e2a22f50622c
Florian Aucomte <f.aucomte@hotmail.co.uk>
2025-03-13 11:42:16 +0000

    Upgrade Selenium and try using sleeps again

--- portal/tests/test_independent_student.py
+++ portal/tests/test_independent_student.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import datetime
+import time
 from unittest.mock import ANY, Mock, patch
 
 from common.mail import campaign_ids
@@ -515,6 +516,8 @@ class TestIndependentStudentFrontend(BaseTest):
         assert is_indep_student_join_request_received_message_showing(self.selenium)
 
         page.revoke_join_request()
+
+        time.sleep(5)
 
         assert is_indep_student_join_request_revoked_message_showing(self.selenium)
 
