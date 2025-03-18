b178a60c64623c08cd21f0c4c580966bb581de7b
Florian Aucomte <f.aucomte@hotmail.co.uk>
2025-03-18 00:47:49 +0000

    fix: Bump version

--- portal/handlers.py
+++ portal/handlers.py
@@ -1,9 +1,8 @@
+from common.utils import two_factor_cache_key
 from django.core.cache import cache
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 from django_otp.models import Device
-
-from common.utils import two_factor_cache_key
 
 
 @receiver([post_save, pre_delete])
