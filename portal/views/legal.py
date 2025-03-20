a1a69487d30fd19b0db7d5c96694758faa84b327
Florian Aucomte <f.aucomte@hotmail.co.uk>
2025-03-20 16:48:55 +0000

    feat: Add cookie list (#2421)
    * feat: Add cookie list
    
    * Typo

--- portal/views/legal.py
+++ portal/views/legal.py
@@ -5,7 +5,7 @@ def privacy_notice(request):
     return render(
         request,
         "portal/privacy_notice.html",
-        {"last_updated": "28th February 2025", "last_updated_children": "28th February 2025"},
+        {"last_updated": "19th March 2025", "last_updated_children": "19th March 2025"},
     )
 
 
