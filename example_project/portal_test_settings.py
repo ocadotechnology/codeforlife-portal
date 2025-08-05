2267155c66a020baa7b232ec18935c6a1fb8e293
faucomte97 <f.aucomte@hotmail.co.uk>
2025-08-05 23:20:50 +0100

    Not actually needed in these settings lol

--- example_project/portal_test_settings.py
+++ example_project/portal_test_settings.py
@@ -165,7 +165,6 @@ MIDDLEWARE = [
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "deploy.middleware.tmp_basic_auth.TempBasicAuthMiddleware",
     "deploy.middleware.security.CustomSecurityMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
