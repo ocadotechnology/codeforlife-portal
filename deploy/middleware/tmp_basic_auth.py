41dd00b055a7fbaf65b62ad76b5496115c05bc6f
faucomte97 <f.aucomte@hotmail.co.uk>
2025-08-08 13:04:51 +0100

    fix: Remove unnecessary check in tmp auth middleware

--- deploy/middleware/tmp_basic_auth.py
+++ deploy/middleware/tmp_basic_auth.py
@@ -6,6 +6,7 @@ from portal.app_settings import TMP_AUTH_TOKEN
 class TempBasicAuthMiddleware:
     """
     Middleware used as a basic auth for the new OTP app until we switch to the main domain.
+    Only meant for use in OTP, so include this in MIDDLEWARES only in OTP settings, not GCP.
     Remove this middleware once the switch is done.
     """
 
@@ -18,24 +19,19 @@ class TempBasicAuthMiddleware:
         url = request.build_absolute_uri()
         temp_domain = "https://www.code4life.education"
 
-        # Only run this middleware on a request from the code4life domain
-        if url.startswith(temp_domain):
+        # If the user inputs the token in the URL correctly, set a cookie to the token's value
+        if url == f"{temp_domain}/{TMP_AUTH_TOKEN}":
+            response = redirect(temp_domain)
+            response.set_cookie(
+                key="TMP_AUTH_TOKEN",
+                value=TMP_AUTH_TOKEN,
+            )
+            return response
+        # For any other request in the code4life domain, check that a cookie with the token's value exists.
+        # Redirect to the main site if it doesn't.
+        else:
+            value = request.COOKIES.get("TMP_AUTH_TOKEN")
+            if value is not None and value == TMP_AUTH_TOKEN:
+                return self.get_response(request)
 
-            # If the user inputs the token in the URL correctly, set a cookie to the token's value
-            if url == f"{temp_domain}/{TMP_AUTH_TOKEN}":
-                response = redirect(temp_domain)
-                response.set_cookie(
-                    key="TMP_AUTH_TOKEN",
-                    value=TMP_AUTH_TOKEN,
-                )
-                return response
-            # For any other request in the code4life domain, check that a cookie with the token's value exists.
-            # Redirect to the main site if it doesn't.
-            else:
-                value = request.COOKIES.get("TMP_AUTH_TOKEN")
-                if value is not None and value == TMP_AUTH_TOKEN:
-                    return self.get_response(request)
-
-            return redirect("https://www.codeforlife.education")
-
-        return self.get_response(request)
+        return redirect("https://www.codeforlife.education")
