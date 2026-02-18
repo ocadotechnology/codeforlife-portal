dc90456ab8af418ab4486821173a4e7a29be3a0f
SKairinos <skairinos@gmail.com>
2026-02-18 15:27:44 +0000

    lolz

--- example_project/portal_test_settings.py
+++ example_project/portal_test_settings.py
@@ -60,12 +60,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "legacy_portal",
-        "USER": "root",
-        "PASSWORD": "password",
-        "HOST": "db",
-        "PORT": 5432,
+        "ENGINE": "django.db.backends.sqlite3",  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        "NAME": os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), "db.sqlite3"
+        ),  # Or path to database file if using sqlite3.
         "ATOMIC_REQUESTS": True,
     }
 }
