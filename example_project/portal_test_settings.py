b9f043816773138b834316441942d8d2c4227d63
faucomte97 <f.aucomte@hotmail.co.uk>
2026-02-19 11:47:05 +0000

    Update captcha silence check

--- example_project/portal_test_settings.py
+++ example_project/portal_test_settings.py
@@ -91,7 +91,7 @@ LOGIN_REDIRECT_URL = "/teach/dashboard/"
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-SILENCED_SYSTEM_CHECKS = ["captcha.recaptcha_test_key_error"]
+SILENCED_SYSTEM_CHECKS = ["django_recaptcha.recaptcha_test_key_error"]
 
 INSTALLED_APPS = [
     "game",
