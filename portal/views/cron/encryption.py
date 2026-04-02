14d5f4f5757faf55d16c224b8a02c85de393c1be
SKairinos <skairinos@gmail.com>
2026-04-02 09:47:03 +0000

    test

--- portal/views/cron/encryption.py
+++ portal/views/cron/encryption.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from ...mixins import CronMixin
 
 
-class EncryptPlaintextFieldsView(CronMixin, APIView):
+class EncryptPlaintextFieldsView(APIView):
     def get(self, request):
         call_command("encrypt_plaintext_fields", "user")
         return Response(status=200)
